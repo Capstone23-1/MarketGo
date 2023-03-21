3c7728ab5884b0baa282b34afc1301604fbe6030
ram1119 <pyoram25@gmail.com>
2023-03-21 18:20:07 +0900

    Initial Commit

---
+++ SeeJang/SeeJangApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeeJangApp.swift
+//  SeeJang
+//
+//  Created by ram on 2023/03/21.
+//
+
+import SwiftUI
+
+@main
+struct SeeJangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
