73496bf6c8820c39f676f54f96c2f6c2846fd746
ram1119 <pyoram25@gmail.com>
2023-03-24 17:55:46 +0900

    [Feat]가까운 거리순 시장 검색하는 View 생성

--- SeeJang/Search/ContentView.swift
+++ SeeJang/Search/ContentView.swift
@@ -6,67 +6,118 @@
 //
 
 import SwiftUI
+import UIKit
 
-class MyClass: Identifiable {
-    let id: UUID
-    let name: String
-    let rate: Float?
-    
-    init(name: String,rate: Float?=nil) {
-        self.id = UUID()
-        self.name = name
-        self.rate = rate
-        print("Name: \(self.name), Rating: \(self.rate)")
-        
+//화면 터치시 키보드 숨김
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
-    
-    
 }
+#endif
 
+
+public struct MarketInfo {
+    var marketName: String
+    var rating: Float
+    var distance: Float //단위 km
+}
 struct ContentView: View {
-    @State var searchText = ""
-    @State var myClassArray: [MyClass] = [
-        MyClass(name: "Class A"),
-        MyClass(name: "Class B"),
-        MyClass(name: "Class C",rate: 3.3)
+    @State private var searchText: String = ""
+    @State private var sortOption: Int = 0
+    
+    
+    var classes: [MarketInfo] = [
+        MarketInfo(marketName: "흑석시장", rating: 4.3,distance: 0.8),
+        MarketInfo(marketName: "상도시장", rating: 4.9,distance: 1.2),
+        MarketInfo(marketName: "광장시장", rating: 3.3,distance: 7.7)
     ]
     
-    var filteredMyClassArray: [MyClass] {
-        if searchText.isEmpty {
-            return myClassArray
-        } else {
-            return myClassArray.filter { $0.name.lowercased().contains(searchText.lowercased()) }
+    var sortedClasses: [MarketInfo] {
+        switch sortOption {
+        case 0: return classes.sorted(by: { $0.marketName < $1.marketName })
+        case 1: return classes.sorted(by: { $0.rating > $1.rating })
+        case 2: return classes.sorted(by: { $0.distance > $1.distance })
+        case 9: return classes.sorted(by: { $0.rating < $1.rating })
+        default: return classes
         }
     }
     
     var body: some View {
-        VStack {
-            TextField("Search", text: $searchText)
-                .padding()
-                .background(Color(.systemGray5))
-                .cornerRadius(8)
-                .padding(.horizontal)
-            
-            List(filteredMyClassArray,id :\.name) { myClass in
-                
-                Text(myClass.name)
-//                if let floatValue = myClass.rate{
-//                    if(0.0...5.0).contains(floatValue){
-//                        Text(floatValue)
-//                    }
-//                    else{
-//                        Text("error")
-//                    }
-//                    
-//                } else {
-//                    Text("평가없음")
-//                }
-                
+        NavigationView{
+            VStack {
+                HStack{ // SearchBar(Title:검색기능)
                     
+                    Image(systemName: "magnifyingglass")
+                        .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 0))
+                    TextField("가고싶은 시장을 입력하세요", text: $searchText)
+                        .foregroundColor(.primary)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        
+                        
+                }
+                
+                
+                HStack{
+                    Spacer()
+                    Picker(selection: $sortOption, label: Text("정렬 기준")) {
+                        Text("거리순").tag(2)
+                        Text("평정 높은순").tag(1)
+                        Text("이름순").tag(0)
+                        Text("Rating Low-High").tag(9)
+                    }
+                    .padding(.horizontal)
+                }
+                
+                
+                List(sortedClasses.filter {
+                    searchText.isEmpty ? true : $0.marketName.localizedCaseInsensitiveContains(searchText)
+                }, id: \.marketName) { MarketInfo in
+                    HStack{
+                        Image(systemName:"person.circle")
+                            .resizable()
+                            .frame(width: 40,height: 40)
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20))
+                        VStack(alignment: .leading,spacing: 4) {
+                            Text("\(MarketInfo.marketName)")
+                                .bold()
+                            
+                            HStack{
+                                Text("\(String(format:"%.1f",MarketInfo.distance)) Km")
+                                Spacer()
+                                Image(systemName: "star.fill")
+                                    .foregroundColor(Color.gray.opacity(0.3))
+                                
+                                
+                                Text("\(String(format:"%.1f",MarketInfo.rating))")
+                            }
+                            .font(.system(size:15))
+                            
+                            
+                            
+                        }
+                        
+                        
+                        
+                    }
+                    .listStyle(PlainListStyle())
+                                      //화면 터치시 키보드 숨김 -> SwiftUI에서는 아직 지원x ->UIKit 사용
+                    .onTapGesture {
+                        hideKeyboard()
+                    }
+                    
+                    
+                }
+                
                 
             }
+            
         }
-    }
+            
+        }
+        
+    
 }
 
 
