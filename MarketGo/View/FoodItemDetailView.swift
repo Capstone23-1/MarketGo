c4de6bf12bc8e501efde613667c446d438b753c8
JooHyeonKim <subin_282@naver.com>
2023-04-05 21:10:58 +0900

    [Feat] FoodItemDetailView 제작중.. 상품상세화면

--- MarketGo/View/FoodItemDetailView.swift
+++ MarketGo/View/FoodItemDetailView.swift
@@ -11,37 +11,50 @@ struct FoodItemDetailView: View {
     var fooditem: FoodItem
     
     var body: some View {
-        
-        VStack(alignment: .leading) {
-            Image(fooditem.imageName)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
+        ScrollView{
             
-            
-            Text(fooditem.name)
-                .font(.system(size: 30, weight: .bold))
-                .padding(.leading, 10)
-            
-            //Text(fooditem.storeName).font(.system(size: 20, weight: .bold))
-            Spacer().frame(height: 10)
-            
-            Text("\(fooditem.price)원")
-                .font(.system(size: 30))
-                .padding(.leading, 10)
-            
-            Spacer().frame(height: 10)
-            
-            Text("\(fooditem.storeName) 둘러보기 >")
-                .font(.system(size: 25))
-                .padding(.leading, 10)
-            
-            Spacer().frame(height: 10)
-            HStack {
-                    Spacer()
+            VStack(alignment: .leading) {
+                Image(fooditem.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                
+                Text(fooditem.name)
+                    .font(.system(size: 30, weight: .bold))
+                    .padding(.leading, 10)
+                
+                //Text(fooditem.storeName).font(.system(size: 20, weight: .bold))
+                Spacer().frame(height: 10)
+                
+                Text("\(fooditem.price)원")
+                    .font(.system(size: 25))
+                    .padding(.leading, 10)
+                
+                Spacer().frame(height: 10)
+                
+                Text("\(fooditem.storeName) 둘러보기 >")
+                    .font(.system(size: 25))
+                    .padding(.leading, 10)
+                
+                Spacer().frame(height: 20)
+                HStack {
+                        Spacer()
+                        Button(action: {
+                            // 버튼이 클릭되었을 때 수행할 액션
+                        }, label: {
+                            Text("사장님과 채팅")
+                                .font(.system(size: 16))
+                                .foregroundColor(.white)
+                        })
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(10)
+
                     Button(action: {
                         // 버튼이 클릭되었을 때 수행할 액션
                     }, label: {
-                        Text("사장님과 채팅")
+                        Text("장바구니 담기")
                             .font(.system(size: 16))
                             .foregroundColor(.white)
                     })
@@ -49,21 +62,18 @@ struct FoodItemDetailView: View {
                     .padding()
                     .background(Color.blue)
                     .cornerRadius(10)
+                    Spacer()
+                    
+                    
+                    }
+                
+                Text(" 📉 가격변동추이")
+                    .font(.system(size: 25))
+                    .padding(.leading, 10)
+            }
 
-                Button(action: {
-                    // 버튼이 클릭되었을 때 수행할 액션
-                }, label: {
-                    Text("장바구니 담기")
-                        .font(.system(size: 16))
-                        .foregroundColor(.white)
-                })
-                .frame(maxWidth: .infinity)
-                .padding()
-                .background(Color.blue)
-                .cornerRadius(10)
-                Spacer()
-                }
         }
+        
         
     }
 }
