dc525e2ea7b3343cebe8d7660f5922d23839e120
ram1119 <pyoram25@gmail.com>
2023-04-15 02:31:30 +0900

    [Refactor]주차장맵뷰에마커선택시카메라이동

--- MarketGo/View/Map/ParkingLotMapView.swift
+++ MarketGo/View/Map/ParkingLotMapView.swift
@@ -16,30 +16,39 @@ struct ParkingLotMapView: UIViewRepresentable {
     
     func makeUIView(context: Context) -> NMFNaverMapView {
         let mapView = NMFNaverMapView()
-        
+
         DispatchQueue.main.async {
             if let userLocation = locationManager.userLocation {
                 let nmg = NMGLatLng(lat: userLocation.lat ?? 23, lng: userLocation.lng ?? 44)
                 let cameraUpdate = NMFCameraUpdate(scrollTo: nmg)
-                
+
                 let marketMarker = NMFMarker()
                 marketMarker.iconImage = NMF_MARKER_IMAGE_BLACK
                 marketMarker.iconTintColor = UIColor.red
                 marketMarker.position = nmg
-                
+
                 marketMarker.mapView = mapView.mapView
                 marketMarker.mapView?.moveCamera(cameraUpdate)
             }
             for parkingLot in parkingLots {
                 let marker = NMFMarker()
-                
+
                 marker.position = NMGLatLng(lat: Double(parkingLot.y) ?? 0, lng: Double(parkingLot.x) ?? 0)
                 marker.mapView = mapView.mapView
+                
+                marker.touchHandler = { [weak mapView] (overlay: NMFOverlay) -> Bool in
+                    if let marker = overlay as? NMFMarker {
+                        let cameraUpdate = NMFCameraUpdate(scrollTo: marker.position)
+                        mapView?.mapView.moveCamera(cameraUpdate)
+                    }
+                    return true
+                }
             }
         }
-        
+
         return mapView
     }
+
     
     func updateUIView(_ uiView: NMFNaverMapView, context: Context) {
         // Do nothing
