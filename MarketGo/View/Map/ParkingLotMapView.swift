ae4017c575b2c60a1d63d0ef1c120377c3e0deba
ram1119 <pyoram25@gmail.com>
2023-04-22 19:37:40 +0900

    [Feat]ProgressIndicator 추가

--- MarketGo/View/Map/ParkingLotMapView.swift
+++ MarketGo/View/Map/ParkingLotMapView.swift
@@ -10,50 +10,56 @@ import NMapsMap
 
 
 struct ParkingLotMapView: UIViewRepresentable {
+
     @ObservedObject var locationManager = LocationManager()
     @Binding var parkingLots: [Document]
+    @Binding var selectedParkingLot: Document?
     var cauLocation = CoordinateInfo(lat: 37.505080, lng: 126.9571020)
-    
+    public let mapView = NMFNaverMapView()
     func makeUIView(context: Context) -> NMFNaverMapView {
-        let mapView = NMFNaverMapView()
-
+        
         DispatchQueue.main.async {
             if let userLocation = locationManager.userLocation {
                 let nmg = NMGLatLng(lat: userLocation.lat , lng: userLocation.lng )
                 let cameraUpdate = NMFCameraUpdate(scrollTo: nmg)
-
                 let marketMarker = NMFMarker()
                 marketMarker.iconImage = NMF_MARKER_IMAGE_BLACK
                 marketMarker.iconTintColor = UIColor.red
                 marketMarker.position = nmg
-
                 marketMarker.mapView = mapView.mapView
                 marketMarker.mapView?.moveCamera(cameraUpdate)
             }
             for parkingLot in parkingLots {
                 let marker = NMFMarker()
-
                 marker.position = NMGLatLng(lat: Double(parkingLot.y) ?? 0, lng: Double(parkingLot.x) ?? 0)
                 marker.mapView = mapView.mapView
-                
+                if let selectedParkingLot = selectedParkingLot, let lat = Double(selectedParkingLot.y), let lng = Double(selectedParkingLot.x) {
+                    // 해당 위치로 카메라 이동
+                    let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: lat, lng: lng))
+                    mapView.mapView.moveCamera(cameraUpdate)
+                }
                 marker.touchHandler = { [weak mapView] (overlay: NMFOverlay) -> Bool in
                     if let marker = overlay as? NMFMarker {
                         let cameraUpdate = NMFCameraUpdate(scrollTo: marker.position)
                         mapView?.mapView.moveCamera(cameraUpdate)
+                        DispatchQueue.main.async {
+                            self.selectedParkingLot = parkingLot
+                        }
+                        
                     }
                     return true
                 }
             }
         }
-
         return mapView
     }
-
     
     func updateUIView(_ uiView: NMFNaverMapView, context: Context) {
-        // Do nothing
+        // 선택된 주차장이 있고, 해당 주차장의 위치 정보가 있는 경우
+        if let selectedParkingLot = selectedParkingLot, let lat = Double(selectedParkingLot.y), let lng = Double(selectedParkingLot.x) {
+            // 해당 위치로 카메라 이동
+            let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: lat, lng: lng))
+            mapView.mapView.moveCamera(cameraUpdate)
+        }
     }
 }
-
-
-
--- MarketGo/View/Map/ParkingLotView.swift
+++ MarketGo/View/Map/ParkingLotView.swift
@@ -6,46 +6,61 @@
 //
 
 import SwiftUI
+import NMapsMap
+
 struct ParkingLotView: View {
     @State private var parkingLots: [Document] = []
     @State private var errorMessage: String?
     @ObservedObject var locationManager = LocationManager()
+    @State private var selectedParkingLot: Document?
+    @State private var isLoading = false // indicator 추가
     
     var body: some View {
         VStack {
             if let errorMessage = errorMessage {
                 Text(errorMessage)
             } else {
-                if parkingLots.isEmpty {
-                    Text("주차장 검색 결과가 없습니다.")
+                if isLoading {
+                    
+                    ProgressView()
+                        .scaleEffect(3.0)
+                        .progressViewStyle(CircularProgressViewStyle(tint: .purple))
+                        .padding()
+
                 } else {
-                    ParkingLotMapView(parkingLots: $parkingLots)
+                    ParkingLotMapView(parkingLots: $parkingLots, selectedParkingLot: $selectedParkingLot)
                     List(parkingLots, id: \.distance) { parkingLot in
                         Text("\(parkingLot.placeName)   \(parkingLot.distance)m")
-                        
+                            .onTapGesture {
+                                selectedParkingLot = parkingLot
+                                print(selectedParkingLot!)
+                            }
+                            .foregroundColor(selectedParkingLot?.id == parkingLot.id ? .blue : .primary)
                     }
                 }
             }
         }
         .onAppear {
             let viewModel = ParkingLotViewModel()
+            isLoading = true // 로딩 시작
             viewModel.searchParkingLot(location: locationManager.userLocation ?? cauLocation, queryKeyword: "주차장") { result in
                 switch result {
-                case .success(let parkingLotData):
-                    DispatchQueue.main.async {
-                        self.parkingLots = parkingLotData.documents
-                    }
-                case .failure(let error):
-                    DispatchQueue.main.async {
-                        self.errorMessage = error.localizedDescription
-                    }
+                    case .success(let parkingLotData):
+                        DispatchQueue.main.async {
+                            self.parkingLots = parkingLotData.documents
+                            isLoading = false // 로딩 종료
+                        }
+                    case .failure(let error):
+                        DispatchQueue.main.async {
+                            self.errorMessage = error.localizedDescription
+                            isLoading = false // 로딩 종료
+                        }
                 }
             }
         }
+        
     }
 }
-
-
 
 
 struct ParkingLotView_Previews: PreviewProvider {
