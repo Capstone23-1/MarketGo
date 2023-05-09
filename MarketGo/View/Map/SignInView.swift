d79476466f61e5b9b8ba4c3a0db95cbb21dbfd3f
ram1119 <pyoram25@gmail.com>
2023-05-10 02:42:54 +0900

    [Refactor]로그인창에 세그먼트 탭 넣어서 상인/소비자 구분 가능

--- MarketGo/View/Map/SignInView.swift
+++ MarketGo/View/Map/SignInView.swift
@@ -13,13 +13,26 @@ struct SignInView: View {
     @State private var showSignUpView = false
     // 로그인 성공 시 MarketSearchView로 전환할지 여부를 결정하는 State 변수
     @State private var moveToMarketSearchView = false
+    // 메인화면 바로가기 버튼 클릭 시 MarketSearchView로 전환할지 여부를 결정하는 State 변수
+    @State private var moveToMarketSearchViewDirectly = false
+    // 소비자와 상인 중 선택된 항목을 저장하는 State 변수
+       @State private var selectedRole = 0
+    // 소비자와 상인을 선택할 수 있는 Picker에 사용할 데이터
+    private let roles = ["소비자", "상인"]
     
     var body: some View {
         GeometryReader { geometry in
             VStack {
                 Spacer()
-                
-                VStack(spacing: 20) { // 텍스트 필드 간 간격을 20으로 설정합니다.
+                VStack(spacing: 20) {
+                    // 소비자와 상인을 선택할 수 있는 Picker
+                    Picker("역할", selection: $selectedRole) {
+                        ForEach(0 ..< 2) {
+                            Text(self.roles[$0])
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    
                     // 이메일 입력 필드
                     TextField("이메일", text: $viewModel.email)
                         .autocapitalization(.none)
@@ -34,6 +47,7 @@ struct SignInView: View {
                         .padding()
                         .background(Color(.systemGray6))
                         .cornerRadius(8)
+                    
                     
                     // 에러 메시지를 표시하는 텍스트 뷰
                     if let error = viewModel.error {
@@ -65,6 +79,7 @@ struct SignInView: View {
                         MarketSearchView()
                     }
                     
+                    
                     // 회원가입 버튼
                     Button(action: {
                         // 버튼 클릭 시 회원가입 창 표시
@@ -76,9 +91,19 @@ struct SignInView: View {
                     .sheet(isPresented: $showSignUpView) {
                         SignUpView()
                     }
+                    // 메인화면 바로가기 버튼
+                    Button(action: {
+                        // 버튼 클릭 시 moveToMarketSearchViewDirectly 상태를 true로 변경하여 MarketSearchView로 전환
+                        self.moveToMarketSearchViewDirectly = true
+                    }) {
+                        Text("메인화면 바로가기")
+                            .foregroundColor(.blue)
+                    }
+                    .fullScreenCover(isPresented: $moveToMarketSearchViewDirectly) {
+                        MarketSearchView()
+                    }
                 }
                 .padding()
-                
                 Spacer()
             }
             .frame(width: geometry.size.width, height: geometry.size.height)
--- MarketGo/View/Map/SignUpView.swift
+++ MarketGo/View/Map/SignUpView.swift
@@ -9,32 +9,27 @@ import SwiftUI
 
 struct SignUpView: View {
     @StateObject private var viewModel = SignUpViewModel()
-    
     var body: some View {
         VStack {
             TextField("이메일", text: $viewModel.email)
-                .autocapitalization(.none)
+                .autocapitalization(.none)//대문자 자동변환 없앰
                 .keyboardType(.emailAddress)
                 .disableAutocorrection(true)
                 .padding()
                 .background(Color(.systemGray6))
                 .cornerRadius(8)
-            
             SecureField("비밀번호", text: $viewModel.password)
                 .padding()
                 .background(Color(.systemGray6))
                 .cornerRadius(8)
-            
             SecureField("비밀번호 확인", text: $viewModel.confirmPassword)
                 .padding()
                 .background(Color(.systemGray6))
                 .cornerRadius(8)
-            
             if let error = viewModel.error {
                 Text(error)
                     .foregroundColor(.red)
             }
-            
             Button(action: {
                 viewModel.signUp { success in
                     if success {
@@ -52,7 +47,6 @@ struct SignUpView: View {
                     .cornerRadius(8)
             }
             .disabled(viewModel.isLoading)
-            
             Spacer()
         }
         .padding()
