7795e58e7619261fb29d9d379fa25cbf32653167
JooHyeonKim <subin_282@naver.com>
2023-05-10 15:43:09 +0900

    [Refactor] 백엔드와의 연동을 위해 구조체 대공사중...살려줘

--- MarketGo/View/MenuItemRow.swift
+++ MarketGo/View/MenuItemRow.swift
@@ -1,54 +1,54 @@
+////
+////  MenuItemRow.swift
+////  MarketGo
+////
+////  Created by 김주현 on 2023/04/09.
+////
 //
-//  MenuItemRow.swift
-//  MarketGo
+//import SwiftUI
 //
-//  Created by 김주현 on 2023/04/09.
 //
-
-import SwiftUI
-
-
-struct MenuItemRow: View {
-    var fooditem:FoodItem
-    
-    var body: some View {
-        
-        HStack {
-            Image("\(fooditem.imageName)")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 70, height: 70)
-            
-            Spacer().frame(width:20)
-            
-            VStack(alignment: .leading, spacing: 10) {
-                Text("\(fooditem.name)")
-                    .font(.system(size: 17))
-                
-                HStack {
-                    Text("\(fooditem.price) 원")
-                        .font(.system(size: 15))
-                        .foregroundColor(.gray)
-                }
-            }
-            
-            Spacer()
-            
-            Image(systemName: "heart.fill")
-                .resizable()
-                .renderingMode(.template)
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 25, height: 25)
-                .foregroundColor(.gray)
-                
-        }
-        .padding()
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        }
-}
-
-struct MenuItemRow_Previews: PreviewProvider {
-    static var previews: some View {
-        MenuItemRow(fooditem: Store.stores[0].products[0])
-    }
-}
+//struct MenuItemRow: View {
+//    var fooditem:FoodItem
+//    
+//    var body: some View {
+//        
+//        HStack {
+//            Image("\(fooditem.imageName)")
+//                .resizable()
+//                .aspectRatio(contentMode: .fit)
+//                .frame(width: 70, height: 70)
+//            
+//            Spacer().frame(width:20)
+//            
+//            VStack(alignment: .leading, spacing: 10) {
+//                Text("\(fooditem.name)")
+//                    .font(.system(size: 17))
+//                
+//                HStack {
+//                    Text("\(fooditem.price) 원")
+//                        .font(.system(size: 15))
+//                        .foregroundColor(.gray)
+//                }
+//            }
+//            
+//            Spacer()
+//            
+//            Image(systemName: "heart.fill")
+//                .resizable()
+//                .renderingMode(.template)
+//                .aspectRatio(contentMode: .fit)
+//                .frame(width: 25, height: 25)
+//                .foregroundColor(.gray)
+//                
+//        }
+//        .padding()
+//        .frame(maxWidth: .infinity, maxHeight: .infinity)
+//        }
+//}
+//
+//struct MenuItemRow_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MenuItemRow(fooditem: Store.stores[0].products[0])
+//    }
+//}
