41c2be34dad3e32f8ee28c311c242fd1f7880d19
JooHyeonKim <subin_282@naver.com>
2023-05-14 16:43:58 +0900

    [Refactor] 구조체 대공사 성공 >< 상품, 가게 서버에서 가져오는거 잘 됨.

--- MarketGo/View/StoreListView.swift
+++ MarketGo/View/StoreListView.swift
@@ -1,90 +1,90 @@
-////
-////  StoreListView.swift
-////  MarketGo
-////
-////  Created by 김주현 on 2023/05/05.
-////
 //
-//import SwiftUI
+//  StoreListView.swift
+//  MarketGo
 //
-//struct StoreListView: View {
-//    @State var stores: [StoreElement] = []
-//    @State var marketId: Int = 17
-//    @ObservedObject var storeModel = StoreViewModel()
-//    @State private var searchText = ""
-//    @StateObject var goodsViewModel = GoodsViewModel() // 상품 정보 뷰 모델
+//  Created by 김주현 on 2023/05/05.
 //
-//    var body: some View {
-//        NavigationView {
-//            ScrollView {
-//                HStack {
-//                    TextField("시장 이름으로 검색", text: $searchText)
-//                        .textFieldStyle(RoundedBorderTextFieldStyle())
-//                    Image(systemName: "magnifyingglass")
-//                }
-//                .padding(.horizontal, 16)
-//                .padding(.top, 16)
-//
-//                Divider()
-//
-//                LazyVStack {
-//                    ForEach(storeModel.stores.sorted { $0.ratings > $1.ratings }.filter {
-//                        searchText.isEmpty ? true : $0.name.contains(searchText)
-//                    }, id: \.id) { store in
-//                        NavigationLink(destination: StoreView(store: store)) {
-//                            HStack {
-//                                VStack {
-//                                    if let fileData = store.fileData {
-//                                        URLImage(url: URL(string: fileData.uploadFileURL))
-//                                    } else {
-//                                        Text("Loading...")
-//                                    }
-//                                }
-//                                .onAppear {
-//                                    viewModel.getFileData(fileId: store.file)
-//                                }
-//
-//                                VStack(alignment: .leading, spacing: 10) {
-//                                    Text(store.name)
-//                                        .font(.headline)
-//                                        .foregroundColor(.black)
-//
-//                                    Text("작성된 리뷰 \(store.ratings)개 > ")
-//                                        .font(.subheadline)
-//                                        .foregroundColor(.black)
-//                                }
-//                                Spacer()
-//                                HStack {
-//                                    Image(systemName: "star.fill")
-//                                        .foregroundColor(.yellow)
-//                                    Text(String(format: "%.1f", store.ratings))
-//                                        .font(.subheadline)
-//                                        .foregroundColor(.black)
-//                                }
-//                            }
-//                            .padding()
-//                            
-//                            // Display goods information for the store
-//                            VStack(alignment: .leading) {
-//                                if let goodsStore = store.storeMarketID {
-//                                    Text("상품 정보:")
-//                                        .font(.headline)
-//                                        .foregroundColor(.black)
-//                                        .padding(.top)
-//                                    ForEach(goodsViewModel.goods.filter { $0.goodsStore?.storeID == goodsStore.storeID }, id: \.id) { goods in
-//                                        Text(goods.goodsName ?? "")
-//                                            .foregroundColor(.black)
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//            .onAppear {
-//                storeModel.fetchStores(marketId: marketId)
-//                goodsViewModel.fetchGoods(forStoreMarketID: marketId)
-//            }
-//        }
-//    }
-//}
+
+import SwiftUI
+
+struct StoreListView: View {
+    @StateObject var storeModel = StoreViewModel()
+    @StateObject var goodsViewModel = GoodsViewModel()
+    @State private var searchText = ""
+
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                HStack {
+                    TextField("시장 이름으로 검색", text: $searchText)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Image(systemName: "magnifyingglass")
+                }
+                .padding(.horizontal, 16)
+                .padding(.top, 16)
+
+                Divider()
+
+                LazyVStack {
+                    ForEach(storeModel.stores.sorted { $0.storeRatings ?? 0 > $1.storeRatings ?? 0 }.filter {
+                        searchText.isEmpty ? true : $0.storeName?.contains(searchText) ?? false
+                    }) { store in
+                        NavigationLink(destination: StoreView(store: store)) {
+                            HStack {
+                                if let storeFile = store.storeFile,
+                                   let uploadFileURL = storeFile.uploadFileURL,
+                                   let url = URL(string: uploadFileURL) {
+                                    URLImage(url: url)
+                                        .frame(width: 80, height: 80)
+                                        .cornerRadius(1)
+                                } else {
+                                    Text("Loading...")
+                                }
+
+                                VStack(alignment: .leading, spacing: 10) {
+                                    Text(store.storeName ?? "")
+                                        .font(.headline)
+                                        .foregroundColor(.black)
+
+                                    Text("작성된 리뷰 \(store.storeRatings ?? 0)개 > ")
+                                        .font(.subheadline)
+                                        .foregroundColor(.black)
+                                }
+
+                                Spacer()
+
+                                HStack {
+                                    Image(systemName: "star.fill")
+                                        .foregroundColor(.yellow)
+                                    Text(String(format: "%.1f", store.storeRatings ?? 0))
+                                        .font(.subheadline)
+                                        .foregroundColor(.black)
+                                }
+                            }
+                            .padding()
+
+                            // Display goods information for the store
+                            VStack(alignment: .leading) {
+                                if let storeMarketID = store.storeMarketID {
+                                    Text("상품 정보:")
+                                        .font(.headline)
+                                        .foregroundColor(.black)
+                                        .padding(.top)
+                                    ForEach(goodsViewModel.goods.filter { $0.goodsStore?.storeID == storeMarketID.marketID }, id: \.id) { goods in
+                                        Text(goods.goodsName ?? "")
+                                            .foregroundColor(.black)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationBarHidden(true)
+            .onAppear {
+                storeModel.fetchStores(marketId: 17) // Provide the desired marketId here
+                goodsViewModel.fetchGoods(forStoreMarketID: 17) // Provide the desired marketId here
+            }
+        }
+    }
+}
