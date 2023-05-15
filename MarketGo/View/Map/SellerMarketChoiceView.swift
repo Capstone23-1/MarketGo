a7c39b386184aa684f30fe25517fc8370f0dc0a6
ram1119 <pyoram25@gmail.com>
2023-05-15 21:36:43 +0900

    [Refactor]SellerMarketChoiceViewModel 파일 생성

--- MarketGo/View/Map/SellerMarketChoiceView.swift
+++ MarketGo/View/Map/SellerMarketChoiceView.swift
@@ -5,41 +5,8 @@
 //  Created by ram on 2023/05/14.
 //
 
-import Foundation
 import SwiftUI
-import Alamofire
-import Combine
 
-class SellerMarketChoiceViewModel: ObservableObject {
-    @Published var markets = MarketArray()
-    @Published var searchText = ""
-    @Published var selectedLocation = "서울"
-    @Published var selectedMarket: MarketOne?
-    
-    
-    
-    
-    init() {
-        getMarkets()
-    }
-    
-    func getMarkets() {
-        
-        let letter = makeStringKoreanEncoded("\(selectedLocation)")
-        let url = "http://3.34.33.15:8080/market/loc/\(letter)"
-        
-        AF.request(url, method: .get).validate().responseDecodable(of: MarketArray.self) { (response) in
-            switch response.result {
-                case .success(let markets):
-                    DispatchQueue.main.async {
-                        self.markets = markets
-                    }
-                case .failure(let error):
-                    print(error.localizedDescription)
-            }
-        }
-    }
-}
 struct SellerMarketChoiceView: View {
     @ObservedObject var viewModel = SellerMarketChoiceViewModel()
     @Binding var selectedMarket: MarketOne?  // 외부에서 전달받은 선택된 마켓 정보를 저장할 바인딩 변수
---
+++ MarketGo/ViewModel/SellerMarketChoiceViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  SellerMarketChoiceViewModel.swift
+//  MarketGo
+//
+//  Created by ram on 2023/05/15.
+//
+
+
+import SwiftUI
+import Alamofire
+import Combine
+
+class SellerMarketChoiceViewModel: ObservableObject {
+    @Published var markets = MarketArray()
+    @Published var searchText = ""
+    @Published var selectedLocation = "서울"
+    @Published var selectedMarket: MarketOne?
+    
+    
+    
+    
+    init() {
+        getMarkets()
+    }
+    
+    func getMarkets() {
+        
+        let letter = makeStringKoreanEncoded("\(selectedLocation)")
+        let url = "http://3.34.33.15:8080/market/loc/\(letter)"
+        
+        AF.request(url, method: .get).validate().responseDecodable(of: MarketArray.self) { (response) in
+            switch response.result {
+                case .success(let markets):
+                    DispatchQueue.main.async {
+                        self.markets = markets
+                    }
+                case .failure(let error):
+                    print(error.localizedDescription)
+            }
+        }
+    }
+}
