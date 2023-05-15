e105ae140afbf18d51bc1f36434626949f9dc392
ram1119 <pyoram25@gmail.com>
2023-05-15 21:38:47 +0900

    [Refactr]전역함수 페이지로 postMember함수 옮김

--- MarketGo/Model/Config.swift
+++ MarketGo/Model/Config.swift
@@ -6,12 +6,30 @@
 //
 // 이곳은 전역변수를 모아놓는 곳입니다.
 import Foundation
-
+import Alamofire
 class Config{
     public let bundleID: String="com.capstone.MarketGo"
 }
 func makeStringKoreanEncoded(_ string: String) -> String {
     return string.addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed) ?? string
+}
+func postUserMemberInfo(memberInfo: MemberInfo, completion: @escaping (Result<Data, Error>) -> Void) {
+    
+    let headers: HTTPHeaders = [
+        "Content-Type": "application/json"
+    ]
+    let encodeName = makeStringKoreanEncoded(memberInfo.memberName!)
+
+    var requestURL = "http://3.34.33.15:8080/member?memberId=&memberToken=\(memberInfo.memberToken ?? "null")&memberName=\(encodeName)&interestMarket=0&cartId=0&storeId=0&recentLatitude=0&recentLongitude=0"
+    print(requestURL)
+    AF.request(requestURL, method: .post, parameters: memberInfo, encoder: JSONParameterEncoder.default, headers: headers).responseData { response in
+        switch response.result {
+        case .success(let data):
+            completion(.success(data))
+        case .failure(let error):
+            completion(.failure(error))
+        }
+    }
 }
 // MARK: - Encode/decode helpers
 
