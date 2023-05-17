b7cb723af6ee80f099373118eb24bb016c3d5c94
ram1119 <pyoram25@gmail.com>
2023-05-17 21:51:06 +0900

    [Feat]SellerSignUpIn 바꿈

--- MarketGo/View/Map/SellerSignIn.swift
+++ MarketGo/View/Map/SellerSignIn.swift
@@ -1,29 +1,120 @@
-////
-////  SellerSignInView.swift
-////  MarketGo
-////
-////  Created by ram on 2023/05/10.
-////
 //
+//  SellerSignInView.swift
+//  MarketGo
+//
+//  Created by ram on 2023/05/10.
+//
+
+import SwiftUI
+
+// TODO: 로그인 성공시 현위치를 받음
+struct SellerSignInView: View {
+    
+    @Binding var moveToProfileView: Bool
+    @ObservedObject private var viewModel = UserSignInViewModel()
+    @Binding var showSignUpView: Bool
+    @State var moveToMarketSearchView = false
+    @State var currentUser: MemberInfo? = nil
+    @EnvironmentObject var userViewModel: UserModel
+    
+    
+    var body: some View {
+        
+        
+        VStack {
+            
+            VStack(spacing: 20) {
+                
+                
+                // 이메일 입력 필드
+                TextField("이메일", text: $viewModel.email)
+                    .autocapitalization(.none)
+                    .keyboardType(.emailAddress)
+                    .disableAutocorrection(true)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                
+                // 비밀번호 입력 필드
+                SecureField("비밀번호", text: $viewModel.password)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                
+                
+                // 에러 메시지를 표시하는 텍스트 뷰
+                if let error = viewModel.error {
+                    Text(error)
+                        .foregroundColor(.red)
+                }
+                
+                // 로그인 버튼
+                Button(action: {
+                    // 버튼 클릭 시 로그인 시도
+                    viewModel.SignIn(userViewModel: userViewModel) { success in
+                        if success {
+                            // 로그인 성공 시 moveToMarketSearchView 상태를 true로 변경하여 MarketSearchView로 전환
+                            self.moveToMarketSearchView = true
+                        } else {
+                            print("로그인 실패")
+                        }
+                    }
+                }) {
+                    Text("로그인")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.accentColor)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                .disabled(viewModel.isLoading) // 로딩 중일 때는 버튼 비활성화
+                .fullScreenCover(isPresented: $moveToMarketSearchView) {
+                    MarketSearchView()
+                }
+                
+                
+                // 회원가입 버튼
+                Button(action: {
+                    // 버튼 클릭 시 회원가입 창 표시
+                    showSignUpView.toggle()
+                }) {
+                    Text("회원가입")
+                        .foregroundColor(.blue)
+                }
+                .sheet(isPresented: $showSignUpView) {
+                    SellerSignUpView()
+                }
+                
+                
+            }
+            .padding()
+            
+            
+            
+            
+        }
+    }
+}
+
 //import SwiftUI
 //
 //struct SellerSignIn: View {
-//    
+//
 //    @Binding var moveToProfileView: Bool
 //    @ObservedObject private var viewModel = UserSignInViewModel()
 ////    @Binding var showSignUpView: Bool
 //    @State var moveToMarketSearchView = false
-//    
-//    
-//    
+//
+//
+//
 //    var body: some View {
-//        
-//        
+//
+//
 //        VStack {
-//            
+//
 //            VStack(spacing: 20) {
-//                
-//                
+//
+//
 //                // 이메일 입력 필드
 //                TextField("이메일", text: $viewModel.email)
 //                    .autocapitalization(.none)
@@ -32,20 +123,20 @@
 //                    .padding()
 //                    .background(Color(.systemGray6))
 //                    .cornerRadius(8)
-//                
+//
 //                // 비밀번호 입력 필드
 //                SecureField("비밀번호", text: $viewModel.password)
 //                    .padding()
 //                    .background(Color(.systemGray6))
 //                    .cornerRadius(8)
-//                
-//                
+//
+//
 //                // 에러 메시지를 표시하는 텍스트 뷰
 //                if let error = viewModel.error {
 //                    Text(error)
 //                        .foregroundColor(.red)
 //                }
-//                
+//
 //                // 로그인 버튼
 //                Button(action: {
 //                    // 버튼 클릭 시 로그인 시도
@@ -69,20 +160,20 @@
 //                .fullScreenCover(isPresented: $moveToMarketSearchView) {
 ////                    MarketSearchView()
 //                }
-//                
-//                
-//                
+//
+//
+//
 //                NavigationLink(destination:SellerSignUpView()){
 //                    Text("회원가입")
 //                }
-//                
-//                
+//
+//
 //            }
 //            .padding()
-//            
-//            
-//            
-//            
+//
+//
+//
+//
 //        }
 //    }
 //}
