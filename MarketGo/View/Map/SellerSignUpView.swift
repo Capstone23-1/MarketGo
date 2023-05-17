c6d52b82f76c67205bb381ef2485250aa31abce5
ram1119 <pyoram25@gmail.com>
2023-05-17 22:55:12 +0900

    [Feat]멤버인포모델 수정

--- MarketGo/View/Map/SellerSignUpView.swift
+++ MarketGo/View/Map/SellerSignUpView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import FirebaseAuth
 
 struct SellerSignUpView: View {
-    @StateObject private var viewModel = UserSignUpViewModel()
+    @StateObject private var viewModel = SellerSignUpViewModel()
     @State private var moveToSignInView = false
     @State private var moveToCoiceView = false
     @State private var selectedMarket: MarketOne? // 선택된 마켓 정보를 저장할 상태 변수
@@ -32,8 +32,7 @@ struct SellerSignUpView: View {
                     .padding()
                     .background(Color(.systemGray6))
                     .cornerRadius(8)
-                    
-
+                
                 Button(action: {
                     self.moveToCoiceView = true
                 }) {
@@ -48,54 +47,52 @@ struct SellerSignUpView: View {
                     SellerMarketChoiceView(selectedMarket: $selectedMarket, isPresented: $moveToCoiceView, marketName: $marketName)
                 }
             }
-                
-                
-                
-                
-                TextField("이메일", text: $viewModel.email)
-                    .autocapitalization(.none)
-                    .keyboardType(.emailAddress)
-                    .disableAutocorrection(true)
-                    .padding()
-                    .background(Color(.systemGray6))
-                    .cornerRadius(8)
-                
-                SecureField("비밀번호", text: $viewModel.password)
-                    .padding()
-                    .background(Color(.systemGray6))
-                    .cornerRadius(8)
-                SecureField("비밀번호 확인", text: $viewModel.confirmPassword)
-                    .padding()
-                    .background(Color(.systemGray6))
-                    .cornerRadius(8)
-                if let error = viewModel.error {
-                    Text(error)
-                        .foregroundColor(.red)
-                }
-                
-                Button(action: {
-                    viewModel.signUp { success in
-                        if success {
-                            print("회원가입 성공, uid: \(viewModel.uid ?? "N/A")")
-                            self.moveToSignInView = true
-                        } else {
-                            print("회원가입 실패")
-                        }
-                    }
-                }) {
-                    Text("회원가입")
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.accentColor)
-                        .foregroundColor(.white)
-                        .cornerRadius(8)
-                }
-                .disabled(viewModel.isLoading)
-                .fullScreenCover(isPresented: $moveToSignInView) {
-                    SignInView()
-                }
+            
+            
+            TextField("이메일", text: $viewModel.email)
+                .autocapitalization(.none)
+                .keyboardType(.emailAddress)
+                .disableAutocorrection(true)
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+            
+            SecureField("비밀번호", text: $viewModel.password)
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+            SecureField("비밀번호 확인", text: $viewModel.confirmPassword)
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+            if let error = viewModel.error {
+                Text(error)
+                    .foregroundColor(.red)
             }
-            .padding()
+            
+            Button(action: {
+                viewModel.signUp { success in
+                    if success {
+                        print("회원가입 성공, uid: \(viewModel.uid ?? "N/A")")
+                        self.moveToSignInView = true
+                    } else {
+                        print("회원가입 실패")
+                    }
+                }
+            }) {
+                Text("회원가입")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.accentColor)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+            .disabled(viewModel.isLoading)
+            .fullScreenCover(isPresented: $moveToSignInView) {
+                SignInView()
+            }
         }
+        .padding()
     }
+}
 
--- MarketGo/ViewModel/SellerSignUpViewModel.swift
+++ MarketGo/ViewModel/SellerSignUpViewModel.swift
@@ -39,7 +39,7 @@ class SellerSignUpViewModel: ObservableObject {
                     
                     // 회원가입 성공 시 uid 저장
                     strongSelf.uid = Auth.auth().currentUser?.uid
-                    let newMemberInfo = MemberInfo(memberID: nil, memberToken: strongSelf.uid, memberName: self?.nickName, interestMarket: nil, cartID: nil, storeID: nil, recentLatitude: nil, recentLongitude: nil)
+                    let newMemberInfo = MemberInfo(memberID: nil, memberToken: strongSelf.uid, memberName: self?.nickName, interestMarket: nil, cartID: nil, storeID: 0, recentLatitude: nil, recentLongitude: nil)
                     postUserMemberInfo(memberInfo: newMemberInfo) { result in
                         switch result {
                             case .success(let data):
