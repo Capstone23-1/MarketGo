7f83e79e498b709ccb769a30f412cb47acb6ede2
JooHyeonKim <subin_282@naver.com>
2023-05-18 19:53:20 +0900

    [Feat] 시장별 Review 리스트 보기. 일단 되긴 함.

--- MarketGo/View/StoreReviewListView.swift
+++ MarketGo/View/StoreReviewListView.swift
@@ -1,100 +1,85 @@
-////
-////  StoreReviewListView.swift
-////  MarketGo
-////
-////  Created by 김주현 on 2023/05/04.
-////
 //
-//import SwiftUI
+//  StoreReviewListView.swift
+//  MarketGo
 //
-//struct StoreReviewListView: View {
+//  Created by 김주현 on 2023/05/04.
+//
 
-//    let store: Store
-//    @State private var reviews: [Review] = Review.reviews
-//    @State private var showMoreReviews = false
-//    
-//    @ObservedObject var storeModel = StoreViewModel()
-//    
+import SwiftUI
 
-//    var body: some View {
-//        VStack() {
-//            Text("\(store.store_name) Review")
-//                .font(.title)
-//                .fontWeight(.bold)
-//                .padding()
-//            
-//            ScrollView {
-//                LazyVStack() {
-//                    ForEach(reviews) { review in
-//                        ReviewRow(review: review)
-//                    }
-//                }
-//            }
-//            .padding()
-//            
-//            
-//        }
-//    }
-//    
-//}
-//
-//struct ReviewRow: View {
-//    let review: Review
-//    @State private var image: UIImage? = nil
-//    
-//    var body: some View {
-//        VStack(alignment: .leading) {
-//            HStack(alignment: .top) {
-//                Text("\(review.rating)점")
-//                    .foregroundColor(.white)
-//                    .padding(.horizontal, 5)
-//                    .padding(.vertical, 5)
-//                    .background(Color.yellow)
-//                    .cornerRadius(10)
-//                
-//                HStack(spacing: 0) {
-//                                    ForEach(0..<5) { index in
-//                                        Image(systemName: "star.fill")
-//                                            .foregroundColor(index < review.rating ? .yellow : .gray)
-//                                    }
-//                }.padding(.leading,5)
-//                               
-//                Text(review.author)
-//                    .font(.headline)
-//                    .fontWeight(.bold)
-//                
-//            }
-//            
-//            Text(review.text)
-//                .font(.body)
-//                .padding(.horizontal, 1)
-//                .padding(.vertical, 7)
-//            
-//            if let imageName = review.imageName {
-//                Image(imageName)
-//                    .resizable()
-//                    .aspectRatio(contentMode: .fit)
-//                    .frame(maxWidth: .infinity, maxHeight:200)
-//                    .padding(.horizontal, 1)
-//                    .padding(.vertical, 3)
-//            }
-//          
-//
-//        }
-//        .padding()
-//        .frame(maxWidth: .infinity, maxHeight: .infinity)
-//        .background(Color.white)
-//        .cornerRadius(5)
-//        .shadow(radius: 2, y: 1)
-//
-//
-//    }
-//}
-//
-//
-//struct StoreReviewListView_Previews: PreviewProvider {
-//    
-//    static var previews: some View {
-//        StoreReviewListView()
-//    }
-//}
+struct StoreReviewListView: View {
+    let store: StoreElement
+    @StateObject private var viewModel = StoreReViewModel()
+    
+    var body: some View {
+        VStack {
+            Text("\(store.storeName ?? "") Review")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding()
+            
+            ScrollView {
+                LazyVStack {
+                    if let reviews = viewModel.reviews {
+                        ForEach(reviews) { review in
+                            ReviewRow(review: review)
+                        }
+                    } else {
+                        ProgressView()
+                    }
+                }
+            }
+            .padding()
+        }
+        .onAppear {
+            viewModel.fetchReviews(for: store.storeID ?? 0)
+        }
+    }
+}
+
+struct ReviewRow: View {
+    let review: StoreReviewElement
+    @State private var image: UIImage? = nil
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .top) {
+                Text("\(review.ratings ?? 0)점")
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 5)
+                    .padding(.vertical, 5)
+                    .background(Color.yellow)
+                    .cornerRadius(10)
+                
+                HStack(spacing: 0) {
+                    ForEach(0..<5) { index in
+                        Image(systemName: "star.fill")
+                            .foregroundColor(index < (review.ratings ?? 0) ? .yellow : .gray)
+                    }
+                }
+                .padding(.leading, 5)
+                
+                Text(review.memberID?.memberName ?? "")
+                    .font(.headline)
+                    .fontWeight(.bold)
+            }
+            
+            Text(review.reviewContent ?? "")
+                .font(.body)
+                .padding(.horizontal, 1)
+                .padding(.vertical, 7)
+            
+            VStack(alignment: .leading) {
+                
+                GoodsImage(url: URL(string: review.storeReviewFile?.uploadFileURL ?? ""), placeholder: Image(systemName: "photo"))
+                
+                
+                }
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.white)
+        .cornerRadius(5)
+        .shadow(radius: 2, y: 1)
+    }
+}
