a42584e54eda700a0f4da0b5039ab9e2742485e4
ram1119 <pyoram25@gmail.com>
2023-05-20 21:08:02 +0900

    [Feat]판매자회원가입 진짜 된다귱

--- MarketGo/Model/Config.swift
+++ MarketGo/Model/Config.swift
@@ -11,24 +11,32 @@ import Alamofire
 
 class Config{
     public let bundleID: String="com.capstone.MarketGo"
-    func postSellerMemberInfo(memberInfo: MemberInfo, completion: @escaping (Result<Data, Error>) -> Void) {
-        
-        let headers: HTTPHeaders = [
-            "Content-Type": "application/json"
-        ]
-        let encodeName = makeStringKoreanEncoded(memberInfo.memberName)
+    
 
-        let requestURL = "http://3.34.33.15:8080/member?memberId=&memberToken=\(memberInfo.memberToken ?? "null")&memberName=\(encodeName)&interestMarket=\(String(describing: memberInfo.interestMarket))&cartId=0&storeId=\(String(describing: memberInfo.storeID!))&recentLatitude=0&recentLongitude=0)"
-    //    print(requestURL)
-        AF.request(requestURL, method: .post, parameters: memberInfo, encoder: JSONParameterEncoder.default, headers: headers).responseData { response in
-            switch response.result {
-            case .success(let data):
-                completion(.success(data))
-            case .failure(let error):
-                completion(.failure(error))
+    func postSellerMemberInfo(memberInfo :MemberInfo, completion: @escaping (Result<Data, AFError>) -> Void) {
+        let url = "http://3.34.33.15:8080/member"
+        let parameters: [String: Any] = [
+            "memberToken": memberInfo.memberToken,
+            "memberName": memberInfo.memberName,
+            "interestMarket": memberInfo.interestMarket,
+            "cartId": 0,
+            "storeId": memberInfo.storeID!,
+            "recentLatitude": 0,
+            "recentLongitude": 0,
+        ]
+
+        AF.request(url, method: .post, parameters: parameters)
+            .validate()
+            .response { response in
+                switch response.result {
+                case .success(let data):
+                    completion(.success(data ?? Data()))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
             }
-        }
     }
+
     func postUserMemberInfo(memberInfo: MemberInfo, completion: @escaping (Result<Data, Error>) -> Void) {
         
         let headers: HTTPHeaders = [
