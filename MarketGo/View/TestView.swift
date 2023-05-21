96241ab4528770e4f24f0c3e1e0eac8b0fb43a73
JooHyeonKim <subin_282@naver.com>
2023-05-21 23:18:40 +0900

    [Feat] 리뷰작성페이지 만들었는데 서버에 내용이 안올라가네..ㅠ이유를 모르겠다

--- MarketGo/View/TestView.swift
+++ MarketGo/View/TestView.swift
@@ -1,49 +1,94 @@
 import SwiftUI
 import Alamofire
 
-
-struct StoreReviewTestView: View {
-    @StateObject private var viewModel = StoreReviewViewModel()
-
+struct MarketReviewSubmissionView: View {
+    @State private var marketId: Int = 0
+    @State private var memberId: Int = 0
+    @State private var ratings: Double = 0.0
+    @State private var reviewContent: String = ""
+    @State private var marketReviewFile: Int = 0
+    
+    @StateObject private var viewModel = MarketReviewPostViewModel()
+    
     var body: some View {
         VStack {
-            if viewModel.isLoading {
-                ProgressView()
-            } else if let storeReviews = viewModel.storeReviews {
-                ForEach(storeReviews) { review in
-                    Text(review.reviewContent ?? "")
-                }
-            } else {
-                Text("No reviews found")
+            TextField("Market ID", value: $marketId, formatter: NumberFormatter())
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            TextField("Member ID", value: $memberId, formatter: NumberFormatter())
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            TextField("Ratings", value: $ratings, formatter: NumberFormatter())
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            TextField("Review Content", text: $reviewContent)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            TextField("Market Review File ID", value: $marketReviewFile, formatter: NumberFormatter())
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            Button("Submit") {
+                viewModel.submitMarketReview(marketId: marketId, memberId: memberId, ratings: ratings, reviewContent: reviewContent, marketReviewFile: marketReviewFile)
             }
+            .padding()
+            .disabled(viewModel.isLoading)
         }
+        .alert(isPresented: $viewModel.showingAlert) {
+            Alert(title: Text("Error"), message: Text(viewModel.errorMessage), dismissButton: .default(Text("OK")))
+        }
+        .padding()
         .onAppear {
-            viewModel.fetchStoreReviews()
+            viewModel.reset()
         }
     }
 }
 
-struct StoreReviewTestView_Previews: PreviewProvider {
+struct MarketReviewSubmissionView_Previews: PreviewProvider {
     static var previews: some View {
-        StoreReviewTestView()
+        MarketReviewSubmissionView()
     }
 }
 
-class StoreReviewViewModel: ObservableObject {
-    @Published var storeReviews: [StoreReviewElement]?
+class MarketReviewPostViewModel: ObservableObject {
     @Published var isLoading = false
+    @Published var showingAlert = false
+    @Published var errorMessage = ""
     
-    func fetchStoreReviews() {
+    func submitMarketReview(marketId: Int, memberId: Int, ratings: Double, reviewContent: String, marketReviewFile: Int) {
         isLoading = true
         
-        let url = URL(string: "http://3.34.33.15:8080/storeReview/all")!
+        let parameters: [String: Any] = [
+            "marketId": marketId,
+            "memberId": memberId,
+            "ratings": ratings,
+            "reviewContent": reviewContent,
+            "marketReviewFile": marketReviewFile
+        ]
         
-        AF.request(url).responseDecodable(of: StoreReview.self) { response in
+        let url = URL(string: "http://3.34.33.15:8080/marketReview")!
+        
+        AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default).response { response in
             self.isLoading = false
             
-            if case .success(let storeReviews) = response.result {
-                self.storeReviews = storeReviews
+            switch response.result {
+            case .success:
+                // Successful submission, do any additional handling if needed
+                break
+            case .failure(let error):
+                self.errorMessage = error.localizedDescription
+                self.showingAlert = true
             }
         }
+    }
+    
+    func reset() {
+        isLoading = false
+        showingAlert = false
+        errorMessage = ""
     }
 }
