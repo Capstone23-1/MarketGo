0eaa880773b063515cb76431b73bb30e39d1c284
JooHyeonKim <subin_282@naver.com>
2023-05-22 19:52:07 +0900

    [Refactor] marketReviewPost 뷰모델 추가

--- MarketGo/Model/MarketReviewPostModel.swift
+++ MarketGo/Model/MarketReviewPostModel.swift
@@ -11,6 +11,7 @@
 //   let marketReviewPost = try? JSONDecoder().decode(MarketReviewPost.self, from: jsonData)
 
 import Foundation
+import Alamofire
 
 // MARK: - MarketReviewPost
 struct MarketReviewPost: Codable {
@@ -25,3 +26,22 @@ struct MarketReviewPost: Codable {
         case ratings, reviewContent, marketReviewFile
     }
 }
+
+
+class MarketReviewPostViewModel: ObservableObject {
+    func submitMarketReview(reviewPost: MarketReviewPost, completion: @escaping (Result<Void, Error>) -> Void) {
+        let apiUrl = "http://3.34.33.15:8080/marketReview"
+
+        AF.request(apiUrl, method: .post, parameters: reviewPost, encoder: JSONParameterEncoder.default)
+            .validate()
+            .response { response in
+                switch response.result {
+                case .success:
+                    completion(.success(()))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+    }
+}
+
