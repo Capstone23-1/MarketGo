ba91d27c6014ef0c1d7d3969a2d7d2f8c7bae098
JooHyeonKim <subin_282@naver.com>
2023-05-22 19:53:54 +0900

    [Refactor] marketreviewpostview 다시

--- MarketGo/View/Map/MarketReviewPostView.swift
+++ MarketGo/View/Map/MarketReviewPostView.swift
@@ -6,234 +6,108 @@
 //
 
 import SwiftUI
-import FirebaseAuth
-import Alamofire
+
 struct MarketReviewPostView: View {
-    @State private var selectedImage: UIImage? = nil // 선택된 이미지를 저장할 변수
-    @State private var isLoading1: Bool = false
-    @State private var isLoading2: Bool = false
-    @StateObject private var viewModel = SellerSignUpViewModel()
-    @EnvironmentObject private var storePost: StorePostViewModel
-    @State private var moveToSignInView = false
-    @State private var moveToCoiceView = false
-    @State private var moveToWriteView = false
-    @State private var selectedMarket: MarketOne? // 선택된 마켓 정보를 저장할 상태 변수
-    @State private var marketName: String = "" // TextField에 바인딩할 변수
-    @State private var newStore: StoreElement?
-    @State private var imageCate = StoreCategory(categoryID: 3,categoryName: "store")
-    @State private var newImage = FileInfo()
-    @State private var imageUploader = ImageUploader()
-    @State var storeName = ""
-    @State private var confirmPasswordMatch: Bool = false
-    @State private var passwordValid: Bool = false // 패스워드 유효성을 확인하는 변수
+    @State private var marketID: Int = 0
+    @State private var memberID: Int = 0
+    @State private var ratings: Double = 0.0
+    @State private var reviewContent: String = ""
+    @State private var marketReviewFile: Int = 0
+    @State private var isLoading: Bool = false
+    @State private var showAlert: Bool = false
+    @State private var alertMessage: String = ""
+    
+    @ObservedObject private var viewModel = MarketReviewPostViewModel()
     
     var body: some View {
         NavigationView {
-            ZStack{
-                Form {
-                    ImageUploadView(category: $imageCate.categoryName, did: $imageCate.categoryID, selectedImage: $selectedImage, newImage: $newImage)
-                    
-                    TextField("가게명", text: $storeName)
-                        .autocapitalization(.none)
-                        .padding()
-                        .background(Color(.systemGray6))
-                        .cornerRadius(8)
-                    
-                    
-                    HStack{
-                        TextField("소속시장", text: $marketName)
-                            .autocapitalization(.none)
-                            .padding()
-                            .background(Color(.systemGray6))
-                            .cornerRadius(8)
-                        
-                        Button(action: {
-                            self.moveToCoiceView = true
-                        }) {
-                            Text("찾기")
-                                .frame(maxWidth: 50)
-                                .padding()
-                                .background(Color.accentColor)
-                                .foregroundColor(.white)
-                                .cornerRadius(8)
-                        }
-                        .sheet(isPresented: $moveToCoiceView,onDismiss: {
-                            print("marketId:\(String(describing: selectedMarket?.marketID))")
-                        }) {
-                            SellerMarketChoiceView(selectedMarket: $selectedMarket, isPresented: $moveToCoiceView, marketName: $marketName)
-                        }
-                    }
-                    Section(header:Text("추가적인 가게 정보를 입력해주세요")){
-                        Button(action: {
-                            self.moveToWriteView = true
-                        }) {
-                            Text("가게정보입력")//입력되지않으면 회원가입이 안되도록
-                                .background(Color.white)
-                                .foregroundColor(.accentColor)
-                                .cornerRadius(8)
-                                .frame(maxWidth: .infinity)
-                        }
-                        .sheet(isPresented: $moveToWriteView, onDismiss: {
-                            printStorePost()
-                        }) {
-                            StoreEnrollView(storeName: $storeName)
-                        }
-                    }
-                    
-                    
-                    
-                    
-                    Section(header:Text("이메일")){
-                        TextField("go@market.com", text: $viewModel.email)
-                            .autocapitalization(.none)
-                            .disableAutocorrection(true)
-                            .padding()
-                            .background(Color(.systemGray6))
-                            .cornerRadius(8)
-                        
-                    }
-                    Section(header: Text("6글자 이상의 비밀번호를 입력해주세요")) {
-                        SecureField("비밀번호", text: $viewModel.password)
-                            .autocapitalization(.none)
-                            .padding()
-                            .disableAutocorrection(true)
-                            .background(Color(.systemGray6))
-                            .cornerRadius(8)
-                            .onChange(of: viewModel.password) { newValue in
-                                passwordValid = newValue.count >= 6 // 비밀번호가 6자 이상인지 확인
-                                confirmPasswordMatch = newValue == viewModel.confirmPassword // 비밀번호와 비밀번호 확인이 같은지 확인
-                            }
-                            .overlay(
-                                HStack {
-                                    Spacer()
-                                    Image(systemName: passwordValid ? "checkmark.circle.fill" : "xmark.circle.fill")
-                                        .foregroundColor(passwordValid ? .green : .red)
-                                }.padding(.horizontal)
-                            )
-                        
-                        SecureField("비밀번호 확인", text: $viewModel.confirmPassword)
-                            .padding()
-                            .disableAutocorrection(true)
-                            .autocapitalization(.none)
-                            .background(Color(.systemGray6))
-                            .cornerRadius(8)
-                            .onChange(of: viewModel.confirmPassword) { newValue in
-                                confirmPasswordMatch = newValue == viewModel.password // 비밀번호와 비밀번호 확인이 같은지 확인
-                            }
-                            .overlay(
-                                HStack {
-                                    Spacer()
-                                    Image(systemName: confirmPasswordMatch ? "checkmark.circle.fill" : "xmark.circle.fill")
-                                        .foregroundColor(confirmPasswordMatch ? .green : .red)
-                                }.padding(.horizontal)
-                            )
-                    }
-                    if let error = viewModel.error {
-                        Text(error)
-                            .foregroundColor(.red)
-                    }
-                    
-                    Button {
-                        Task {
-                            await processSignUp()
-                        }
-                    } label: {
-                        Text("회원가입")
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color.accentColor)
-                            .foregroundColor(.white)
-                            .cornerRadius(8)
-                    }
-                    .disabled(isLoading1)
-                    .fullScreenCover(isPresented: $moveToSignInView) {
-                        SignInView()
-                    }
-                    
-                    
+            Form {
+                Section(header: Text("Market Information")) {
+                    TextField("Market ID", value: $marketID, formatter: NumberFormatter())
+                        .keyboardType(.numberPad)
+                    TextField("Member ID", value: $memberID, formatter: NumberFormatter())
+                        .keyboardType(.numberPad)
                 }
                 
-                if isLoading2 {
-                    ProgressView()
-                        .scaleEffect(2)
-                        .progressViewStyle(CircularProgressViewStyle(tint: .blue))
-                        .frame(width: 100, height: 100)
-                        .background(Color.white.opacity(0.8))
-                        .cornerRadius(20)
-                        .shadow(radius: 10)
-                }
-            }
-            .onAppear {
-                isLoading2 = false
-            }
-        }
-        
-        
-    }
-    
-    func processSignUp() async {
-        isLoading1 = true
-        do {
-            if let image = self.selectedImage {
-                let result = try await imageUploader.uploadImageToServer(image: image, category: imageCate.categoryName, id: String(imageCate.categoryID))
-                print("이미지업로드성공:\(String(describing: result.uploadFileName!))")
-                isLoading2 = true
-                if let id = result.fileID {
-                    storePost.storeFile = id
-                    print("file id get : \(storePost.storeFile) id: \(id)")
-                    storePost.marketId=selectedMarket!.marketID
-                }
-            } else {
-                print("이미지를 선택하지 않았습니다.")
-                return
-            }
-            
-            let enroll = try await storePost.enrollStore { result in
-                switch result {
-                    case .success(let storeElement):
-                        viewModel.storeId = storeElement.storeID!
-                        viewModel.storeMarketId=selectedMarket!.marketID
-                        print("vm.marketId: \(viewModel.storeMarketId)")
-                        viewModel.nickName=storeElement.storeName!
-                        
-                        print("-------")
-                        print(viewModel.storeId)
-                    case .failure(let error):
-                        // 에러가 발생했을 때의 동작
-                        print("Error enrolling store: \(error)")
-                }
-            }
-            
-            DispatchQueue.main.async {
-                viewModel.signUp { (success, error) in
-                    if success {
-                        print("회원가입 성공, uid: \(viewModel.uid ?? "N/A")")
-                        self.moveToSignInView = true
-                    } else {
-                        print("회원가입 실패: \(error?.localizedDescription ?? "")")
+                Section(header: Text("Review")) {
+                    HStack {
+                        Text("Ratings")
+                        Slider(value: $ratings, in: 0...5, step: 0.5)
+                        Text(String(format: "%.1f", ratings))
                     }
-                    isLoading1 = false
-                    isLoading2 = false
+                    TextEditor(text: $reviewContent)
+                        .frame(height: 100)
                 }
+                
+                Button(action: {
+                    submitReview()
+                }, label: {
+                    Text("Submit")
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                })
+                .disabled(isLoading)
             }
-        } catch {
-            print("Error uploading image: \(error)")
-            isLoading1 = false
+            .navigationBarTitle("Market Review")
+            .alert(isPresented: $showAlert, content: {
+                Alert(title: Text("Notification"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+            })
         }
     }
     
-    
-    func printStorePost() {
-        print("storeName: \(storePost.storeName)")
-        print("storeAddress1: \(storePost.storeAddress1)")
-        print("storeCategory: \(storePost.storeCategory)")
-        print("storePhonenum: \(storePost.storePhonenum)")
-        print("storeInfo: \(storePost.storeInfo)")
-        print("cardAvail: \(storePost.cardAvail)")
-        print("localAvail: \(storePost.localAvail)")
+    func submitReview() {
+        guard marketID != 0 else {
+            showAlert(message: "Please enter Market ID.")
+            return
+        }
+        
+        guard memberID != 0 else {
+            showAlert(message: "Please enter Member ID.")
+            return
+        }
+        
+        guard ratings > 0 else {
+            showAlert(message: "Please select Ratings.")
+            return
+        }
+        
+        guard !reviewContent.isEmpty else {
+            showAlert(message: "Please enter Review Content.")
+            return
+        }
+        
+        isLoading = true
+        
+        let reviewPost = MarketReviewPost(
+            mrMarketID: marketID,
+            mrMemberID: memberID,
+            ratings: ratings,
+            reviewContent: reviewContent,
+            marketReviewFile: marketReviewFile
+        )
+        
+        viewModel.submitMarketReview(reviewPost: reviewPost) { result in
+            switch result {
+            case .success:
+                showAlert(message: "Review submitted successfully.")
+            case .failure(let error):
+                showAlert(message: "Failed to submit review. \(error.localizedDescription)")
+            }
+            isLoading = false
+        }
     }
-    func isPasswordValid() -> Bool {
-        return viewModel.password.count >= 6 && viewModel.password == viewModel.confirmPassword
-    }
     
+    func showAlert(message: String) {
+        alertMessage = message
+        showAlert = true
+    }
+}
+
+struct MarketReviewPostView_Previews: PreviewProvider {
+    static var previews: some View {
+        MarketReviewPostView()
+    }
 }
