dd0a60ceb7a391082bd4304df69873974ef6c383
ram1119 <pyoram25@gmail.com>
2023-05-24 01:23:05 +0900

    [Refactor]상품리스트뷰 완성

--- MarketGo/View/Map/SellerTempView.swift
+++ MarketGo/View/Map/SellerTempView.swift
@@ -10,11 +10,13 @@ import SwiftUI
 struct SellerTempView: View {
     @EnvironmentObject var userViewModel: UserModel
     @EnvironmentObject var marketModel: MarketModel
-    @State var move = false
+    @State var move1 = false
+    @State var move2 = false
+    @State var move3 = false
     var body: some View {
         VStack{
             Button {
-                move=true
+                move1=true
             } label: {
                 Text("정보수정창")
                     .frame(maxWidth: .infinity)
@@ -23,7 +25,7 @@ struct SellerTempView: View {
                     .foregroundColor(.white)
                     .cornerRadius(8)
             }
-            .sheet(isPresented: $move) {
+            .sheet(isPresented: $move1) {
                 if let storeElement = userViewModel.currentUser?.storeID {
                     StoreUpdateView(obse: ObservableStoreElement(storeElement: storeElement))
 
@@ -33,11 +35,38 @@ struct SellerTempView: View {
                     Text("No store data available")
                 }
             }
+            Button {
+                move2=true
+            } label: {
+                Text("상품 등록 창")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.accentColor)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+            .sheet(isPresented: $move2) {
+                GoodsPostView()
+                
+            }
+            Button {
+                move3=true
+            } label: {
+                Text("상품 리스트 창")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.accentColor)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+            .sheet(isPresented: $move3) {
+                GoodsSellerView()
+                
+            }
             
         }
         
         
     }
 }
-
 
