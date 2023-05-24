288abf7cbc2005efc8281fe9b86a7c6ec08c6d18
ram1119 <pyoram25@gmail.com>
2023-05-25 01:33:47 +0900

    [Feat]QR code 저장하는 버튼과 메서드 완성

--- MarketGo/View/Map/qrView.swift
+++ MarketGo/View/Map/qrView.swift
@@ -1,19 +1,48 @@
 import SwiftUI
 import QRCode
-
 struct qrView: View {
     @State private var qrCodeString = "marketgo://98" // QR 생성 데이터
     @State private var info = "싱글탱글생성회"
+    @State private var showingAlert = false // 알림창 표시 여부를 결정하는 State
     
     var body: some View {
         VStack {
             Text("자세한 가게 정보를 보려면 QR 코드를 인식해주세요")
-                .font(.largeTitle) // 대문자 폰트로 변경
+                .font(.largeTitle)
             QRCodeView(qrCodeString: $qrCodeString)
             Text(info)
-                .font(.title) // 커다란 제목 폰트로 변경
-                .frame(maxWidth: .infinity, alignment: .center) // 가로 방향으로 정중앙 정렬
+                .font(.title)
+                .frame(maxWidth: .infinity, alignment: .center)
                 .padding()
+            Button(action: {
+                saveImageToCameraRoll()
+            }) {
+                Text("Save QR Code")
+                    .font(.title2)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text("Saved!"), message: Text("Your QR code has been saved to your Photos."), dismissButton: .default(Text("OK")))
+            }
+        }
+    }
+    
+    // QR 코드 이미지를 카메라 롤에 저장
+    private func saveImageToCameraRoll() {
+        var qrCode = QRCode(string: qrCodeString)
+        qrCode?.size = CGSize(width: 200, height: 200)
+        qrCode?.scale = 1.0
+        qrCode?.inputCorrection = .quartile
+
+        if let image = try? qrCode?.image() { // Add 'try?' here
+            let uiImage = UIImage(cgImage: image.cgImage!)
+            UIImageWriteToSavedPhotosAlbum(uiImage, nil, nil, nil)
+            showingAlert = true // 성공 알림창을 표시
+        } else {
+            print("Failed to generate QR code image")
         }
     }
 }
@@ -32,11 +61,10 @@ struct QRCodeView: View {
 
         var uiImage: UIImage?
 
-        do {
-            let image = try qrCode?.image() // Now using 'try'
-            uiImage = UIImage(cgImage: image!.cgImage!)
-        } catch {
-            print("Failed to create QR code image: \(error)")
+        if let image = try? qrCode?.image() { // Add 'try?' here
+            uiImage = UIImage(cgImage: image.cgImage!)
+        } else {
+            print("Failed to create QR code image")
         }
 
         // If the image failed to create, this will just display an empty image
