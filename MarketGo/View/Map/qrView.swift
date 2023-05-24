eb5ed2c92da4a6d98f68a183770c9455a4cc0947
ram1119 <pyoram25@gmail.com>
2023-05-25 01:43:47 +0900

    큐알코드 저장에 실패하고 있다

--- MarketGo/View/Map/qrView.swift
+++ MarketGo/View/Map/qrView.swift
@@ -1,10 +1,12 @@
 import SwiftUI
 import QRCode
+import UIKit
+
 struct qrView: View {
     @State private var qrCodeString = "marketgo://98" // QR 생성 데이터
     @State private var info = "싱글탱글생성회"
     @State private var showingAlert = false // 알림창 표시 여부를 결정하는 State
-    
+
     var body: some View {
         VStack {
             Text("자세한 가게 정보를 보려면 QR 코드를 인식해주세요")
@@ -28,29 +30,23 @@ struct qrView: View {
                 Alert(title: Text("Saved!"), message: Text("Your QR code has been saved to your Photos."), dismissButton: .default(Text("OK")))
             }
         }
+        .background(SnapshotView(showingAlert: $showingAlert))
     }
-    
+
     // QR 코드 이미지를 카메라 롤에 저장
     private func saveImageToCameraRoll() {
-        var qrCode = QRCode(string: qrCodeString)
-        qrCode?.size = CGSize(width: 200, height: 200)
-        qrCode?.scale = 1.0
-        qrCode?.inputCorrection = .quartile
-
-        if let image = try? qrCode?.image() { // Add 'try?' here
-            let uiImage = UIImage(cgImage: image.cgImage!)
-            UIImageWriteToSavedPhotosAlbum(uiImage, nil, nil, nil)
+        if let snapshotImage = SnapshotView(showingAlert: $showingAlert).snapshot() {
+            UIImageWriteToSavedPhotosAlbum(snapshotImage, nil, nil, nil)
             showingAlert = true // 성공 알림창을 표시
         } else {
-            print("Failed to generate QR code image")
+            print("Failed to generate snapshot image")
         }
     }
 }
 
 struct QRCodeView: View {
-    
     @Binding var qrCodeString: String
-    
+
     var body: some View {
         var qrCode = QRCode(string: qrCodeString)
         qrCode?.color = UIColor.black
@@ -72,5 +68,67 @@ struct QRCodeView: View {
             .resizable()
             .aspectRatio(contentMode: .fit)
             .frame(width: 200, height: 200)
+    }
+}
+
+struct SnapshotView: UIViewRepresentable {
+    typealias UIViewType = UIView
+
+    @Binding var showingAlert: Bool
+    private var snapshotImage: UIImage?
+
+    func makeUIView(context: Context) -> UIView {
+        return UIView()
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {
+        if let uiView = uiView as? SnapshotUIView {
+            uiView.updateSnapshot(snapshotImage, showingAlert: $showingAlert)
+        }
+    }
+
+    func snapshot() -> UIImage? {
+        guard let uiView = UIApplication.shared.windows.first?.rootViewController?.view else {
+            return nil
+        }
+
+        UIGraphicsBeginImageContextWithOptions(uiView.bounds.size, false, 0)
+        defer { UIGraphicsEndImageContext() }
+
+        if let context = UIGraphicsGetCurrentContext() {
+            uiView.layer.render(in: context)
+            let snapshotImage = UIGraphicsGetImageFromCurrentImageContext()
+            self.snapshotImage = snapshotImage
+            return snapshotImage
+        }
+
+        return nil
+    }
+}
+
+class SnapshotUIView: UIView {
+    private var snapshotImageView: UIImageView?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSnapshotImageView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupSnapshotImageView()
+    }
+
+    private func setupSnapshotImageView() {
+        let imageView = UIImageView()
+        addSubview(imageView)
+        imageView.frame = bounds
+        imageView.contentMode = .scaleAspectFill
+        snapshotImageView = imageView
+    }
+
+    func updateSnapshot(_ snapshotImage: UIImage?, showingAlert: Binding<Bool>) {
+        snapshotImageView?.image = snapshotImage
+        showingAlert.wrappedValue = true
     }
 }
