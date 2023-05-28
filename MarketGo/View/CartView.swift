7ddd1b2cbe408c8b7052d54c18e76f518c79c74b
JooHyeonKim <subin_282@naver.com>
2023-05-28 13:36:51 +0900

    [Refactor] addProduct 함수 만들었다. 장바구니에 상품추가하는 함수

--- MarketGo/View/CartView.swift
+++ MarketGo/View/CartView.swift
@@ -16,4 +16,20 @@ class cart: ObservableObject {
    @Published var cartItems: [CartItem]
    init() {
       self.cartItems = []
-}}
+   }
+    
+    func addProduct(product: GoodsOne){
+       var addNewProduct = true
+       for (index, item) in cartItems.enumerated() {
+          if item.product.id == product.id {
+             cartItems[index].count = cartItems[index].count + 1
+             addNewProduct = false
+          }
+       }
+       if addNewProduct {
+          cartItems.append(CartItem(product: product, count: 1))
+       }
+    }
+}
+
+
