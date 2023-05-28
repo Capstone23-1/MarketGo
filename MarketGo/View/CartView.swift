b49f6dd0b46ae0d87910ad187c555e150aa2d361
JooHyeonKim <subin_282@naver.com>
2023-05-28 16:14:11 +0900

    [Refactor] 장바구니 데이터 fetch 성공적!

--- MarketGo/View/CartView.swift
+++ MarketGo/View/CartView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import Alamofire
 
 struct CartItem: Identifiable {
     var id: UUID
@@ -13,36 +14,108 @@ struct CartItem: Identifiable {
 }
 
 class cart: ObservableObject {
-   @Published var cartItems: [CartItem]
-   init() {
-      self.cartItems = []
-   }
+    @Published var cart: Cart?
+    @Published var cartItems: [CartItem] = []
     
-    func addProduct(product: GoodsOne){
-       var addNewProduct = true
-       for (index, item) in cartItems.enumerated() {
-          if item.product.id == product.id {
-             cartItems[index].count = cartItems[index].count + 1
-             addNewProduct = false
-          }
-       }
-       if addNewProduct {
-          cartItems.append(CartItem(product: product, count: 1))
-       }
+    func fetchCart(forUserId cartId: Int) {
+        let url = "http://3.34.33.15:8080/cart/\(cartId)"
+        
+        AF.request(url).responseData { response in
+            switch response.result {
+            case .success(let data):
+                do {
+                    let decoder = JSONDecoder()
+                    let cart = try decoder.decode(Cart.self, from: data)
+                    DispatchQueue.main.async {
+                        self.cart = cart
+                        self.updateCartItems()
+                    }
+                } catch {
+                    print(error.localizedDescription)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    private func updateCartItems() {
+        guard let cart = self.cart else {
+            self.cartItems = []
+            return
+        }
+        
+        var items: [CartItem] = []
+        
+        if let goodsId1 = cart.goodsId1, let unit1 = cart.unit1, goodsId1.goodsID != 0 {
+            let cartItem = CartItem(product: goodsId1, count: unit1)
+            items.append(cartItem)
+        }
+        
+        if let goodsId2 = cart.goodsId2, let unit2 = cart.unit2, goodsId2.goodsID != 0 {
+            let cartItem = CartItem(product: goodsId2, count: unit2)
+            items.append(cartItem)
+        }
+        
+        if let goodsId3 = cart.goodsId3, let unit3 = cart.unit3, goodsId3.goodsID != 0 {
+            let cartItem = CartItem(product: goodsId3, count: unit3)
+            items.append(cartItem)
+        }
+        
+        if let goodsId4 = cart.goodsId4, let unit4 = cart.unit4, goodsId4.goodsID != 0 {
+            let cartItem = CartItem(product: goodsId4, count: unit4)
+            items.append(cartItem)
+        }
+        
+        if let goodsId5 = cart.goodsId5, let unit5 = cart.unit5, goodsId5.goodsID != 0 {
+            let cartItem = CartItem(product: goodsId5, count: unit5)
+            items.append(cartItem)
+        }
+        
+        if let goodsId6 = cart.goodsId6, let unit6 = cart.unit6, goodsId6.goodsID != 0 {
+            let cartItem = CartItem(product: goodsId6, count: unit6)
+            items.append(cartItem)
+        }
+
+        if let goodsId7 = cart.goodsId7, let unit7 = cart.unit7, goodsId7.goodsID != 0 {
+            let cartItem = CartItem(product: goodsId7, count: unit7)
+            items.append(cartItem)
+        }
+
+        if let goodsId8 = cart.goodsId8, let unit8 = cart.unit8, goodsId8.goodsID != 0 {
+            let cartItem = CartItem(product: goodsId8, count: unit8)
+            items.append(cartItem)
+        }
+
+        if let goodsId9 = cart.goodsId9, let unit9 = cart.unit9, goodsId9.goodsID != 0 {
+            let cartItem = CartItem(product: goodsId9, count: unit9)
+            items.append(cartItem)
+        }
+
+        if let goodsId10 = cart.goodsId10, let unit10 = cart.unit10, goodsId10.goodsID != 0 {
+            let cartItem = CartItem(product: goodsId10, count: unit10)
+            items.append(cartItem)
+        }
+        
+        self.cartItems = items
     }
 }
 
-
 struct CartView: View {
-   @EnvironmentObject var cart: cart
+    @EnvironmentObject var userModel: UserModel
+    @EnvironmentObject var cart: cart
+
    var body: some View {
-   NavigationView {
-      List($cart.cartItems) { $cartItem in
-         NavigationLink(destination: CartItemDetail(cartItem: $cartItem)) {
-            CartItemRow(cartItem:  $cartItem)}
-      }.navigationTitle("Cart")
-   }.navigationViewStyle(StackNavigationViewStyle())
+
+       List($cart.cartItems) { $cartItem in
+          NavigationLink(destination: CartItemDetail(cartItem: $cartItem)) {
+             CartItemRow(cartItem:  $cartItem)}
+       }
+       .onAppear{
+           cart.fetchCart(forUserId: userModel.currentUser?.cartID?.cartID ?? 0)
+       }
 }}
+
 
 
 struct CartItemRow: View {
