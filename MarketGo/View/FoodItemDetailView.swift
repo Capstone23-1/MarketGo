8f6fe992c3072f412496f7b672fe55185037d0ca
JooHyeonKim <subin_282@naver.com>
2023-05-28 23:42:48 +0900

    [Refactor] RoundedButton 추가

--- MarketGo/View/FoodItemDetailView.swift
+++ MarketGo/View/FoodItemDetailView.swift
@@ -62,3 +62,13 @@ struct FoodItemDetailView: View {
     }
 }
 
+struct RoundedButton: View {
+   var imageName: String
+   var text: String
+   var body: some View {
+   HStack {
+      Image(systemName: imageName).font(.title)
+      Text(text).fontWeight(.semibold).font(.title)
+   }.padding().foregroundColor(.white).background(Color.orange)
+.cornerRadius(40)
+}}
