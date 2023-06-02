b2445eae283badf1994fbce07cc0d3dbd66ebfbb
ram1119 <pyoram25@gmail.com>
2023-06-03 02:15:38 +0900

    [Bug]셀러홈뷰갈때 버튼 한개 안보이던거 수정

--- MarketGo/View/SellerHomeView.swift
+++ MarketGo/View/SellerHomeView.swift
@@ -4,26 +4,140 @@ import SwiftUI
 struct SellerHomeView: View {
     @EnvironmentObject var userModel: UserModel
     @State var storeID:StoreElement?
+    @State var isLoading = false
     let rad = CGFloat(30)
     var body: some View {
         NavigationView(){
             ScrollView {
-                VStack() {
-                    
-                    HStack {
-                        if let storeID = storeID {
-                            NavigationLink(destination: EditStoreView(obse: ObservableStoreElement(storeElement: storeID))) {
-                                VStack{
-                                    Image(systemName: "")
-                                        .resizable()
-                                        .foregroundColor(.gray)
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 50, height: 50)
-                                        .padding(.bottom, 8)
-                                    Text("회원정보수정창")
-                                        .font(.title)
-                                        .fontWeight(.bold)
+                ZStack{
+                    VStack() {
+                        
+                        HStack {
+                            if let storeID = storeID {
+                                NavigationLink(destination: EditStoreView(obse: ObservableStoreElement(storeElement: storeID))) {
+                                    VStack{
+                                        Image(systemName: "")
+                                            .resizable()
+                                            .foregroundColor(.gray)
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 50, height: 50)
+                                            .padding(.bottom, 8)
+                                        Text("회원정보수정창")
+                                            .font(.title)
+                                            .fontWeight(.bold)
+                                    }
                                 }
+                                .frame(maxWidth: .infinity, maxHeight: 200)
+                                .frame(height: 200)
+                                .background(Color.white)
+                                .cornerRadius(rad)
+                                .frame(maxWidth: .infinity)
+                                .padding(.all,10)
+                            }
+                            else{
+                                
+                                NavigationLink(destination: EmptyView()) {
+                                    VStack{
+                                        Image(systemName: "")
+                                            .resizable()
+                                            .foregroundColor(.gray)
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 50, height: 50)
+                                            .padding(.bottom, 8)
+                                        Text("회원정보수정창")
+                                            .font(.title)
+                                            .fontWeight(.bold)
+                                    }
+                                }
+                                .frame(maxWidth: .infinity, maxHeight: 200)
+                                .frame(height: 200)
+                                .background(Color.white)
+                                .cornerRadius(rad)
+                                .frame(maxWidth: .infinity)
+                                .padding(.all,10)
+                                
+                            }
+                            NavigationLink(destination: PostGoodsView()) {
+                                Text("상품\n등록창")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                Image(systemName: "cursorarrow.rays")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 40)
+                            }
+                            .frame(maxWidth: .infinity, maxHeight: 200)
+                            .frame(height: 200)
+                            .background(Color.white)
+                            .cornerRadius(rad)
+                            .frame(maxWidth: .infinity)
+                            .padding(.all,10)
+                            
+                        }
+                        .frame(maxWidth: .infinity)
+                        .padding(.all,10)
+                        
+                        HStack {
+                            NavigationLink(destination: PostGoodsView()) {
+                                Text("상품\n등록창")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                Image(systemName: "cursorarrow.rays")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 40)
+                            }
+                            .frame(maxWidth: .infinity, maxHeight: 200)
+                            .frame(height: 200)
+                            .background(Color.white)
+                            .cornerRadius(rad)
+                            .frame(maxWidth: .infinity)
+                            .padding(.all,10)
+                            NavigationLink(destination: GoodsListSellerView()) {
+                                Text("상품 리스트 창")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                Image(systemName: "cursorarrow.rays")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 40)
+                            }
+                            .frame(maxWidth: .infinity, maxHeight: 200)
+                            .frame(height: 200)
+                            .background(Color.white)
+                            .cornerRadius(rad)
+                            .frame(maxWidth: .infinity)
+                            .padding(.all,10)
+                            
+                        }
+                        .frame(maxWidth: .infinity)
+                        .padding(.horizontal,10)
+                        
+                        HStack {
+                            
+                            NavigationLink(destination: DogamQRGenerateView()) {
+                                Text("도감\nQR\n생성")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                Image(systemName: "cursorarrow.rays")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 40)
+                            }
+                            .frame(maxWidth: .infinity, maxHeight: 200)
+                            .frame(height: 200)
+                            .background(Color.white)
+                            .cornerRadius(rad)
+                            .frame(maxWidth: .infinity)
+                            .padding(.all,10)
+                            NavigationLink(destination: QRCodeView()) {
+                                Text("가게\n안내\nQR")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                Image(systemName: "cursorarrow.rays")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 40)
                             }
                             .frame(maxWidth: .infinity, maxHeight: 200)
                             .frame(height: 200)
@@ -32,103 +146,29 @@ struct SellerHomeView: View {
                             .frame(maxWidth: .infinity)
                             .padding(.all,10)
                         }
-                        NavigationLink(destination: PostGoodsView()) {
-                            Text("상품\n등록창")
-                                .font(.title)
-                                .fontWeight(.bold)
-                            Image(systemName: "cursorarrow.rays")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 40)
-                        }
-                        .frame(maxWidth: .infinity, maxHeight: 200)
-                        .frame(height: 200)
-                        .background(Color.white)
-                        .cornerRadius(rad)
                         .frame(maxWidth: .infinity)
-                        .padding(.all,10)
-                        
+                        .padding(.horizontal,10)
                     }
-                    .frame(maxWidth: .infinity)
-                    .padding(.all,10)
-                    
-                    HStack {
-                        NavigationLink(destination: PostGoodsView()) {
-                            Text("상품\n등록창")
-                                .font(.title)
-                                .fontWeight(.bold)
-                            Image(systemName: "cursorarrow.rays")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 40)
-                        }
-                        .frame(maxWidth: .infinity, maxHeight: 200)
-                        .frame(height: 200)
-                        .background(Color.white)
-                        .cornerRadius(rad)
-                        .frame(maxWidth: .infinity)
-                        .padding(.all,10)
-                        NavigationLink(destination: GoodsListSellerView()) {
-                            Text("상품 리스트 창")
-                                .font(.title)
-                                .fontWeight(.bold)
-                            Image(systemName: "cursorarrow.rays")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 40)
-                        }
-                        .frame(maxWidth: .infinity, maxHeight: 200)
-                        .frame(height: 200)
-                        .background(Color.white)
-                        .cornerRadius(rad)
-                        .frame(maxWidth: .infinity)
-                        .padding(.all,10)
-                        
+                    if isLoading {
+                        ProgressView()
+                            .scaleEffect(2)
+                            .progressViewStyle(CircularProgressViewStyle(tint: .blue))
+                            .frame(width: 100, height: 100)
+                            .background(Color.white.opacity(0.8))
+                            .cornerRadius(20)
+                            .shadow(radius: 10)
                     }
-                    .frame(maxWidth: .infinity)
-                    .padding(.horizontal,10)
-                    
-                    HStack {
-                        
-                        NavigationLink(destination: DogamQRGenerateView()) {
-                            Text("도감\nQR\n생성")
-                                .font(.title)
-                                .fontWeight(.bold)
-                            Image(systemName: "cursorarrow.rays")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 40)
-                        }
-                        .frame(maxWidth: .infinity, maxHeight: 200)
-                        .frame(height: 200)
-                        .background(Color.white)
-                        .cornerRadius(rad)
-                        .frame(maxWidth: .infinity)
-                        .padding(.all,10)
-                        NavigationLink(destination: QRCodeView()) {
-                            Text("가게\n안내\nQR")
-                                .font(.title)
-                                .fontWeight(.bold)
-                            Image(systemName: "cursorarrow.rays")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 40)
-                        }
-                        .frame(maxWidth: .infinity, maxHeight: 200)
-                        .frame(height: 200)
-                        .background(Color.white)
-                        .cornerRadius(rad)
-                        .frame(maxWidth: .infinity)
-                        .padding(.all,10)
-                    }
-                    .frame(maxWidth: .infinity)
-                    .padding(.horizontal,10)
                 }
-            }
+                }
             .padding()
             .background(Color.gray.opacity(0.2))
             .onAppear {
-                storeID = userModel.currentUser?.storeID
+                isLoading = true
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    storeID = userModel.currentUser?.storeID
+                    isLoading=false
+                }
+                
             }
         }
     }
