2b13a177a545665c094e9fa39cce3ee7978a0110
ram1119 <pyoram25@gmail.com>
2023-06-04 21:23:33 +0900

    [Refactor]마켓인포리스트에 선없앰

--- MarketGo/View/MarketInfoList.swift
+++ MarketGo/View/MarketInfoList.swift
@@ -1,43 +1,42 @@
 import Foundation
 import SwiftUI
 
-struct MarketListView: View {
+struct MarketInfoList: View {
     @Binding var marketData: MarketOne?
-    
-    @available(iOS 15.0, *)
+
     var body: some View {
-        List {
-            if let marketData = marketData {
-                Section(header: Text("데이터 기준 일자: \(convertDate(from: marketData.updateTime!))").font(.footnote)) {
+        if let marketData = marketData {
+            ScrollView {
+                VStack {
+                    Section(header: Text("데이터 기준 일자: \(convertDate(from: marketData.updateTime!))").font(.footnote)) {
 
-                    CardView(title: "시장 이름", value: marketData.marketName ?? "", iconName: "house")
+                        CardView(title: "시장 이름", value: marketData.marketName ?? "", iconName: "house")
 
-                    CardView(title: "주소", value: marketData.marketAddress1 ?? "", iconName: "mappin.and.ellipse")
+                        CardView(title: "주소", value: marketData.marketAddress1 ?? "", iconName: "mappin.and.ellipse")
 
-                    CardView(title: "평점", value: String(format:"%.1f", marketData.marketRatings ?? 0.0), iconName: "star")
+                        CardView(title: "평점", value: String(format:"%.1f", marketData.marketRatings ?? 0.0), iconName: "star")
 
-                    CardView(title: "상세 정보", value: marketData.marketInfo ?? "", iconName: "info.circle")
+                        CardView(title: "상세 정보", value: marketData.marketInfo ?? "", iconName: "info.circle")
 
-                    CardView(title: "주차장 보유여부", value: marketData.parking ?? "", iconName: "car")
-                    CardView(title: "화장실", value: marketData.toilet ?? "", iconName: "person.crop.square")
-                        
+                        CardView(title: "주차장 보유여부", value: marketData.parking ?? "", iconName: "car")
 
-                    CardView(title: "시장 연락처", value: marketData.marketPhonenum ?? "", iconName: "phone")
-                        
+                        CardView(title: "화장실", value: marketData.toilet ?? "", iconName: "person.crop.square")
 
-                    CardView(title: "지역화페", value: marketData.marketGiftcard ?? "", iconName: "creditcard")
-                        
+                        CardView(title: "시장 연락처", value: marketData.marketPhonenum ?? "", iconName: "phone")
+
+                        CardView(title: "지역화페", value: marketData.marketGiftcard ?? "", iconName: "creditcard")
+                    }
                 }
-            } else {
-                Text("데이터를 불러오는 데 실패했습니다.")
-                    .foregroundColor(.red)
-                    .font(.headline)
             }
+            .padding() // Adding padding for better spacing
+            .background(Color.white)
+        } else {
+            Text("데이터를 불러오는 데 실패했습니다.")
+                .foregroundColor(.red)
+                .font(.headline)
         }
-        .listStyle(GroupedListStyle())
-        .background(Color.white)
     }
-    
+
     func convertDate(from string: String) -> String {
         let inputFormatter = DateFormatter()
         inputFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
@@ -80,7 +79,6 @@ struct CardView: View {
         .background(Color.white)
         .cornerRadius(20)
         .shadow(color: .gray, radius: 2, x: 0, y: 2)
-        .frame(maxWidth: .infinity)
         .padding(.horizontal)
     }
 }
