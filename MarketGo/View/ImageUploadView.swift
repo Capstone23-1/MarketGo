7428b212d929437cf8e0846424e70a2d31b83c19
ram1119 <pyoram25@gmail.com>
2023-06-06 04:36:39 +0900

    [Feat]기본이미지 제공하도록 변경

--- MarketGo/View/ImageUploadView.swift
+++ MarketGo/View/ImageUploadView.swift
@@ -2,21 +2,54 @@ import SwiftUI
 import Alamofire
 import UIKit
 
+struct DefaultImageView: View {
+    @Binding var selectedImage: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+
+    // Basic fruit images
+    let fruitImages = ["apple", "banana", "broccoli", "carrot", "grape", "melon", "orange", "pineapple", "potato", "tomato", "watermelon"]
+        .compactMap { UIImage(named: $0) }
+
+    var body: some View {
+        let columns = [
+            GridItem(.flexible()),
+            GridItem(.flexible()),
+            GridItem(.flexible())
+        ]
+
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(fruitImages, id: \.self) { image in
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                        .onTapGesture {
+                            selectedImage = image
+                            presentationMode.wrappedValue.dismiss()
+                        }
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+
 struct ImageUploadView: View {
-    @State var showImagePicker: Bool = false
     @State var image: UIImage?
     @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
     @Binding var category: String
-    
-  
+    @State private var showDefaultImagePicker: Bool = false
+    @State private var showImagePicker: Bool = false
+
     let imageUploader = ImageUploader()
     @Binding var selectedImage: UIImage? // 선택된 이미지를 저장할 변수
-    
+
     @Binding var newImage: FileInfo
     let imageSize = 100.0
-    
+
     var body: some View {
-        
+
         HStack(alignment:.center){
             Spacer()
             VStack(alignment:.center) {
@@ -37,24 +70,76 @@ struct ImageUploadView: View {
                     .clipShape(Circle())
                     .padding(.top)
             }
-            
-            Button("이미지 선택") {
-                self.showImagePicker = true
-            }
-            .sheet(isPresented: $showImagePicker, onDismiss: {
-                self.showImagePicker = false
-                
-            }) {
-                ImagePickerModel(selectedImage: self.$selectedImage, sourceType: self.sourceType)
-            }
-            
+                HStack{
+                    
+                    // ImageUploadView
+                    Button("기본 이미지 사용") {
+                        showDefaultImagePicker = true
+                    }
+                    .onTapGesture {
+                        showDefaultImagePicker = true
+                        showImagePicker = false
+                    }
+                    .sheet(isPresented: $showDefaultImagePicker) {
+                        DefaultImageView(selectedImage: self.$selectedImage)
+                    }
+
+                    Button("이미지 선택") {
+                        showImagePicker = true
+                    }
+                    .onTapGesture {
+                        showDefaultImagePicker = false
+                        showImagePicker = true
+                    }
+                    .sheet(isPresented: $showImagePicker, onDismiss: {
+                        showDefaultImagePicker = false
+                        showImagePicker = false
+                    }) {
+                        ImagePickerModel(selectedImage: self.$selectedImage, sourceType: self.sourceType)
+                    }
+                }
+
         }.padding()
             Spacer()
         }
     }
 }
+struct ImagePickerModel: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var selectedImage: UIImage?
+    var sourceType: UIImagePickerController.SourceType
 
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = context.coordinator
+        imagePickerController.sourceType = sourceType
+        return imagePickerController
+    }
 
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+    }
 
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
 
+    final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: ImagePickerModel
 
+        init(_ parent: ImagePickerModel) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
