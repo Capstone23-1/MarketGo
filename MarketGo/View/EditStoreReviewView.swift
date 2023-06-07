b5f00204bb0089a9f9a606f63bebda64ab37166e
JooHyeonKim <subin_282@naver.com>
2023-06-07 13:39:53 +0900

    [Feat] 가게리뷰 수정창 생성

---
+++ MarketGo/View/EditStoreReviewView.swift
@@ -0,0 +1,20 @@
+//
+//  EditStoreReviewView.swift
+//  MarketGo
+//
+//  Created by 김주현 on 2023/06/07.
+//
+
+import SwiftUI
+
+struct EditStoreReviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditStoreReviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditStoreReviewView()
+    }
+}
