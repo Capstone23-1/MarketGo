90b82c8a84f21a8a1c9a645ac218764cd65eb983
JooHyeonKim <subin_282@naver.com>
2023-06-07 12:53:43 +0900

    [Refactor] 마켓리뷰 후 페이지 전환

--- MarketGo/View/MarketReviewPutView.swift
+++ MarketGo/View/MarketReviewPutView.swift
@@ -1,15 +1,11 @@
-//
-//  MarketReviewPutView.swift
-//  MarketGo
-//
-//  Created by 김주현 on 2023/06/07.
-//
 import SwiftUI
 import Alamofire
 
 struct MarketReviewPutView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
     @State private var imageUploader = ImageUploader()
-    @State private var imageCate = StoreCategory(categoryID: 6,categoryName: "m_review")
+    @State private var imageCate = StoreCategory(categoryID: 6, categoryName: "m_review")
     @State private var newImage = FileInfo()
     @State private var selectedImage: UIImage? = nil
     @State private var isLoading: Bool = false
@@ -22,18 +18,17 @@ struct MarketReviewPutView: View {
     let starColor = Color(red: 255/255, green: 202/255, blue: 40/255)
     let starWidth: CGFloat = 30.0
     
+    @State private var showAlert = false
+    
     var body: some View {
         VStack {
             Form {
-                
-                ReviewImageUploadView(category: $imageCate.categoryName,  selectedImage: $selectedImage, newImage: $newImage)
+                ReviewImageUploadView(category: $imageCate.categoryName, selectedImage: $selectedImage, newImage: $newImage)
                 
                 Text("\(marketReviewId)")
                 
                 Section(header: Text("Review")) {
-                    
                     HStack(spacing: 10) {
-
                         Text("별점")
                         Spacer()
                         ForEach(0..<5) { index in
@@ -51,8 +46,7 @@ struct MarketReviewPutView: View {
                     TextEditor(text: $reviewContent)
                         .frame(height: 100)
                 }
-                
-            }.onAppear(perform: loadData)
+            }
             
             Button(action: {
                 Task {
@@ -66,21 +60,27 @@ struct MarketReviewPutView: View {
                     .cornerRadius(10)
             }
         }
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text("완료"),
+                message: Text("리뷰 수정이 완료되었습니다."),
+                dismissButton: .default(Text("확인"), action: {
+                    presentationMode.wrappedValue.dismiss()
+                })
+            )
+        }
+        .onAppear(perform: loadData)
     }
     
     func loadData() {
-
         async {
             do {
                 let fileInfo = try await ImageDownloader().fetchImageFileInfo(url: "http://3.34.33.15:8080/file/\(fileId)")
                 selectedImage = try await ImageDownloader().fetchImage(fileInfo: fileInfo)
-                // 사용하려는 이미지가 여기에 있습니다.
             } catch {
-                // 오류 처리
                 print("Failed to fetch image: \(error)")
             }
         }
-
     }
     
     func updateMarketReview() async {
@@ -91,59 +91,32 @@ struct MarketReviewPutView: View {
                 
                 if let id = result.fileID {
                     fileId = id
-                    
                 }
             } else {
                 print("이미지를 선택하지 않았습니다.")
                 return
             }
+        } catch {
+            print("Error uploading image: \(error)")
+            isLoading = false
+            return
         }
-            catch {
-                print("Error uploading image: \(error)")
-                isLoading = false
-            }
-        
         
         let enreviewContent = makeStringKoreanEncoded(reviewContent)
-        let enfileId = String(describing: fileId)
-        //http://3.34.33.15:8080/marketReview/86?ratings=4.0&reviewContent=ㅍㅣㅣ고ㄴ하다&marketReviewFile=311
-
         let urlString = "http://3.34.33.15:8080/marketReview/\(marketReviewId)?ratings=\(ratings)&reviewContent=\(enreviewContent)&marketReviewFile=\(fileId)"
         
         let headers: HTTPHeaders = ["Content-Type": "application/json"]
         
-        
         AF.request(urlString, method: .put, headers: headers)
             .validate(statusCode: 200..<300)
-            .responseDecodable(of: MarketReviewElement.self) {response in
+            .responseDecodable(of: MarketReviewElement.self) { response in
                 switch response.result {
-                    case .success(let updatedReview):
-                        print("Update successful: \(updatedReview)")
-                    case .failure(let error):
-                        print("업로드 실패! \(error)")
+                case .success(let updatedReview):
+                    print("Update successful: \(updatedReview)")
+                    showAlert = true
+                case .failure(let error):
+                    print("업로드 실패! \(error)")
                 }
             }
-        
     }
-    
-    
-//    func updateMarketReview(Id: Int) {
-//        let urlString = "http://3.34.33.15:8080/marketReview/\(Id)"
-//        let parameters: [String: Any] = [
-//            "ratings": ratings,
-//            "reviewContent": reviewContent,
-//            "marketReviewFile": marketReviewFile
-//        ]
-//
-//        AF.request(urlString, method: .put, parameters: parameters, encoding: JSONEncoding.default).response { response in
-//            switch response.result {
-//            case .success:
-//                print("Market review updated successfully.")
-//            case .failure(let error):
-//                print("Error updating market review: \(error)")
-//            }
-//        }
-//    }
-    
-    
 }
