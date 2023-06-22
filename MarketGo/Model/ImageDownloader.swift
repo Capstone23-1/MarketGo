a2a3d1b1457aa488ffbbd059f5aef3732f923d40
ram1119 <pyoram25@gmail.com>
2023-06-22 21:52:30 +0900

    [Refactor]사진 url 없는 경우 else로 처리

--- MarketGo/Model/ImageDownloader.swift
+++ MarketGo/Model/ImageDownloader.swift
@@ -23,17 +23,24 @@ class ImageDownloader {
     
     
     func fetchImage(fileInfo: FileInfo) async throws -> UIImage? {
-        guard let url = URL(string: fileInfo.uploadFileURL!) else { throw URLError(.badURL) }
-        let data = try await withCheckedThrowingContinuation { continuation in
-            AF.request(url).responseData { response in
-                switch response.result {
-                case .success(let data):
-                    continuation.resume(returning: data)
-                case .failure(let error):
-                    continuation.resume(throwing: error)
+        if let fileurl = fileInfo.uploadFileURL {
+            guard let url = URL(string: fileurl) else { throw URLError(.badURL) }
+            let data = try await withCheckedThrowingContinuation { continuation in
+                AF.request(url).responseData { response in
+                    switch response.result {
+                    case .success(let data):
+                        continuation.resume(returning: data)
+                    case .failure(let error):
+                        continuation.resume(throwing: error)
+                    }
                 }
             }
+            return UIImage(data: data)
         }
-        return UIImage(data: data)
+        else{
+            return nil
+        }
+        
+        
     }
 }
